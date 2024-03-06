a55fe051c7e77c3c8b2369e04f179501f6a5205b
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-06 20:20:00 +0900

    init: 프로젝트 초기 설정

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.1.18:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+});
