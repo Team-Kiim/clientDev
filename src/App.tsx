42be65c21a0880332f83c3a5f7f80eaa855ee5da
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-22 14:48:26 +0900

    fix: react-modal 관련 경고 처리
    Modal 컴포넌트를 사용할 때, 다음과 같은 경고가 발생
    
    Warning: react-modal: App element is not defined. Please use `Modal.setAppElement(el)` or set `appElement={el}`. This is needed so screen readers don't see main content when modal is opened. It is not recommended, but you can opt-out by setting `ariaHideApp={false}`.
    
    Modal.setAppElement 정적 메소드를 사용하여 경고 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Modal from 'react-modal';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import router from '@/router.tsx';
+
+Modal.setAppElement('#root');
 
 const queryClient = new QueryClient({
     defaultOptions: {
