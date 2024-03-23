fe71afa2930da77a2051ed7f579f5502fdd9cdde
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-23 23:59:50 +0900

    design: 전체 레이아웃 디자인 수정

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,10 +4,16 @@ import GlobalNavbar from '@/Components/GNB/GlobalNavbar.tsx';
 export default function Layout() {
     return (
         <div>
-            <div>
-                <GlobalNavbar />
-            </div>
-            <div>
+            <header
+                className={
+                    'fixed z-10 flex h-[5rem] w-full flex-col justify-center border-b border-gray-300 bg-white/50 backdrop-blur-md'
+                }
+            >
+                <div className={'mx-64'}>
+                    <GlobalNavbar />
+                </div>
+            </header>
+            <div className={'mx-64 pt-28'}>
                 <Outlet />
             </div>
         </div>
