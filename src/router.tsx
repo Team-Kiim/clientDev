26266aaade324efc14c3e00c7d4a23801ce7c9d0
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-24 00:00:03 +0900

    feat: 메인 페이지 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import AppLayout from '@/layout.tsx';
+import MainPage from '@/Pages/page.tsx';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<AppLayout />}>
             <Route>
-                <Route element={<>홈</>} path={'/'} />
+                <Route element={<MainPage />} path={'/'} />
             </Route>
             <Route>
                 <Route element={<>커뮤니티</>} path={'/community'} />
