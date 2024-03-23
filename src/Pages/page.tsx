304224babbd15351dc728beef6f787d849cadb17
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-24 00:03:22 +0900

    feat: 메인 페이지에 대한 컴포넌트 추가

---
+++ src/Pages/page.tsx
@@ -0,0 +1,9 @@
+import SearchFilter from '@/Components/PostSearchFilter/SearchFilter.tsx';
+
+export default function Page() {
+    return (
+        <div className={'h-[2000px]'}>
+            <SearchFilter />
+        </div>
+    );
+}
