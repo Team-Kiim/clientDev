4fd5b20df06574662b3ab291f4296a9ff1ddf6cf
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-30 18:46:36 +0900

    fix: 검색한 게시글 제목 쿼리 스트링으로 관리하도록 수정

--- src/Components/PostSearch/PostSearch.tsx
+++ src/Components/PostSearch/PostSearch.tsx
@@ -1,15 +1,12 @@
 import { debounce } from 'lodash';
-import { KeyboardEventHandler, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useQueryClient } from '@tanstack/react-query';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { KeyboardEventHandler, useEffect, useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { ArrowPathIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function PostSearch() {
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const navigate = useNavigate();
-
-    const queryClient = useQueryClient();
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = debounce(event => {
         if (event.nativeEvent.isComposing) {
@@ -21,12 +18,17 @@ export default function PostSearch() {
                 return;
             }
             const inputValue = inputRef.current.value;
-            console.log(inputValue);
+            searchParams.set('post_title', inputValue);
+            setSearchParams(searchParams);
         }
     }, 250);
 
+    useEffect(() => {
+        inputRef.current.value = searchParams.get('post_title') ?? '';
+    }, [searchParams]);
+
     return (
-        <div className={'flex w-[30rem] gap-x-2.5 rounded-3xl bg-gray-100 px-4 py-2.5'}>
+        <div className={'flex w-[30rem] items-center gap-x-2.5 rounded-3xl bg-gray-100 px-5 py-2.5'}>
             <MagnifyingGlassIcon className={'size-6'} />
             <input
                 className={'flex-1 bg-gray-100 text-[0.9rem] focus:outline-none'}
@@ -37,6 +39,21 @@ export default function PostSearch() {
                 autoCapitalize={'off'}
                 ref={inputRef}
             />
+            <div className={'tooltip tooltip-bottom'} data-tip={'초기화'}>
+                <button
+                    className={'flex items-center transition-all hover:scale-110'}
+                    type={'button'}
+                    onClick={() => {
+                        if (!searchParams.has('post_title')) {
+                            return;
+                        }
+                        searchParams.delete('post_title');
+                        setSearchParams(searchParams);
+                    }}
+                >
+                    <ArrowPathIcon className={'size-6'} />
+                </button>
+            </div>
         </div>
     );
 }
