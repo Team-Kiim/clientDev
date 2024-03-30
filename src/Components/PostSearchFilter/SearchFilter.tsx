65e00d9187f10240ea73345fd7d5fce7da3ae206
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-30 18:44:29 +0900

    feat: 키워드 검색 기능 추가

--- src/Components/PostSearchFilter/SearchFilter.tsx
+++ src/Components/PostSearchFilter/SearchFilter.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import KeywordSearchInput from '@/Components/PostSearchFilter/KeywordSearchInput.tsx';
 import PostTypeToggleButton from '@/Components/PostSearchFilter/PostTypeToggleButton.tsx';
 
 export default function SearchFilter() {
     return (
         <div className={'flex justify-between'}>
-            <div />
+            <KeywordSearchInput />
             <PostTypeToggleButton />
         </div>
     );
