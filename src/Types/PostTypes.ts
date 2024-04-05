0262f39042e7183e9d910a760b6587663350bf3f
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-05 23:05:36 +0900

    feat: 게시글에 관한 타입 추가

---
+++ src/Types/PostTypes.ts
@@ -0,0 +1,12 @@
+export type Post = {
+    id: number;
+    title: string;
+    username: string;
+    profileImgSrc: string;
+    bodyContent: string;
+    viewCount: number;
+    likeCount: number;
+    createdTime: number[];
+    modifiedTime?: number[];
+    skillCategories?: string[];
+};
