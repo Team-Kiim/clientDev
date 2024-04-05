aadb25148910437223632e632d7ede33e43070f6
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-05 23:03:51 +0900

    feat: 게시글 목록 가져오는 함수 추가

--- src/Pages/page.tsx
+++ src/Pages/page.tsx
@@ -1,9 +1,18 @@
+import { Suspense } from 'react';
 import SearchFilter from '@/Components/PostSearchFilter/SearchFilter.tsx';
+import PostAddButton from '@/Components/Post/PostAddButton.tsx';
+import PostList from '@/Components/Post/PostList.tsx';
 
 export default function Page() {
     return (
-        <div className={'h-[2000px]'}>
+        <div>
             <SearchFilter />
+            <PostAddButton />
+            <div className={'my-12'}>
+                <Suspense fallback={<>Loading...</>}>
+                    <PostList />
+                </Suspense>
+            </div>
         </div>
     );
 }
---
+++ src/Utils/getPostList.ts
@@ -0,0 +1,149 @@
+import axios from 'axios';
+import { faker } from '@faker-js/faker';
+import { QueryFunction } from '@tanstack/react-query';
+import type { Post } from '@/Types/PostTypes.ts';
+
+const testData: Post[] = [
+    {
+        id: 1,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 2,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 3,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 4,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 5,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 6,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 7,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 8,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 9,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+    {
+        id: 10,
+        username: '김강민',
+        profileImgSrc: faker.image.avatar(),
+        title: '게시글 테스트 제목',
+        bodyContent:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        viewCount: 1000,
+        likeCount: 1000,
+        createdTime: [2024, 4, 5],
+        modifiedTime: [2024, 4, 5],
+        skillCategories: ['JavaScript'],
+    },
+];
+
+const getPostList: QueryFunction<Post[], [_1: string, _3: { postType: string; title: string; keyword: string }]> = ({
+    queryKey,
+}) => {
+    const { postType, title, keyword } = queryKey[1];
+    console.log(postType, title, keyword);
+
+    // return axios.get('/api/dev-posts').then(response => response.data);
+    return Promise.resolve(testData);
+};
+
+export default getPostList;
