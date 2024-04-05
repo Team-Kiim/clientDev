b11fbd7b7c12fd166fa37ba3246fe83b49f14ed7
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-05 23:06:41 +0900

    feat: 게시글 작성 버튼 추가

---
+++ src/Components/Post/PostAddButton.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { PlusIcon } from '@heroicons/react/24/outline';
+
+export default function PostAddButton() {
+    const navigate = useNavigate();
+
+    const handlePostAddButtonClick = () => {
+        navigate('/post/write');
+    };
+
+    return (
+        <div className={'tooltip tooltip-top fixed bottom-10 right-40'} data-tip={'게시글 작성'}>
+            <button
+                className={'rounded-full bg-violet-600 p-2'}
+                onClick={() => {
+                    handlePostAddButtonClick();
+                }}
+            >
+                <PlusIcon className={'size-11 text-white'} />
+            </button>
+        </div>
+    );
+}
