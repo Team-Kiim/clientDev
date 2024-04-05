29efdfadf3e6eec59a5d5c38bf6e2f6a1e2a782c
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-05 23:06:19 +0900

    feat: url 이 변경될 때, 화면 최상단으로 스크롤하는 훅 추가

---
+++ src/Hooks/useScrollToTop.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+
+export default function useScrollToTop(): void {
+    const location = useLocation();
+
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        window.requestAnimationFrame(() => {
+            window.scrollTo({ top: 0, left: 0 });
+        });
+    }, [location.pathname, searchParams]);
+}
