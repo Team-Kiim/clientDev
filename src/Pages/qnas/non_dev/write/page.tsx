eebfa71e933404a0cb9c9f3e736a636ecff95cbc
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-09 22:09:20 +0900

    design: 개발, 비개발 작성 페이지 마진 수정

--- src/Pages/qnas/non_dev/write/page.tsx
+++ src/Pages/qnas/non_dev/write/page.tsx
@@ -2,7 +2,7 @@ import NonDevQnAWriteForm from '@/Pages/qnas/non_dev/write/Components/NonDevQnAW
 
 export default function Page() {
     return (
-        <div className={'mx-[32rem]'}>
+        <div className={'mx-[28rem]'}>
             <main>
                 <h1 className={'mx-2 text-[1.65rem] font-bold'}>비개발 Q&A 작성</h1>
                 <NonDevQnAWriteForm />
