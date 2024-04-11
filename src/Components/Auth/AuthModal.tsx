da8432696efed82376e848e04bc579082a79c659
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-11 16:04:36 +0900

    feat: 앱 자체의 로그인, 회원가입 기능 추가

--- src/Components/Auth/AuthModal.tsx
+++ src/Components/Auth/AuthModal.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import SignInButtons from '@/Components/Auth/SignInButtons.tsx';
-import SignUpButtons from '@/Components/Auth/SignUpButtons.tsx';
+import SignInForm from '@/Components/Auth/SignInForm.tsx';
+import SignUpForm from '@/Components/Auth/SignUpForm.tsx';
+import OAuthSection from '@/Components/Auth/OAuthSection/OAuthSection.tsx';
 
 interface Props {
     isAuthModalOpen: boolean;
@@ -26,9 +27,9 @@ export default function AuthModal({ isAuthModalOpen, closeModal }: Props) {
     return (
         <Modal
             isOpen={isAuthModalOpen}
-            style={{ overlay: { backgroundColor: 'rgba(0, 0, 0, 50%)', zIndex: 10 } }}
+            style={{ overlay: { backgroundColor: 'rgba(0, 0, 0, 50%)', zIndex: 60 } }}
             className={
-                'fixed left-1/2 top-1/2 flex w-[25rem] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg border border-white bg-white px-12 py-7 focus:outline-none'
+                'fixed left-1/2 top-1/2 z-[60] flex w-[30rem] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg border border-white bg-white px-12 py-7 focus:outline-none'
             }
             closeTimeoutMS={280}
         >
@@ -51,19 +52,36 @@ export default function AuthModal({ isAuthModalOpen, closeModal }: Props) {
             >
                 KoffeeChat
             </Link>
-            <div className={'flex flex-col items-center'}>
-                {isSignUpMode ? <SignUpButtons /> : <SignInButtons />}
-                <div className={'divider text-gray-400'} />
-                <span
-                    className={
-                        'cursor-pointer text-base font-semibold text-gray-500 transition-all hover:text-violet-600'
-                    }
-                    onClick={() => {
-                        setIsSignUpMode(!isSignUpMode);
-                    }}
-                >
-                    {isSignUpMode ? '소셜 계정으로 로그인' : '소셜 계정으로 회원가입'}
-                </span>
+            <div className={'flex flex-col items-center gap-y-8'}>
+                {isSignUpMode ? (
+                    <div className={'flex w-full flex-col gap-y-2'}>
+                        <SignUpForm />
+                        <div className={'divider text-sm text-gray-500'}>소셜로 회원가입</div>
+                        <OAuthSection />
+                    </div>
+                ) : (
+                    <div className={'flex w-full flex-col gap-y-2'}>
+                        <SignInForm
+                            closeModal={() => {
+                                closeModal();
+                            }}
+                        />
+                        <div className={'divider text-sm text-gray-500'}>소셜로 로그인</div>
+                        <OAuthSection />
+                    </div>
+                )}
+                <div className={'flex w-full justify-end'}>
+                    <span
+                        className={
+                            'cursor-pointer text-[0.9rem] font-semibold text-gray-600 transition-all hover:text-violet-600'
+                        }
+                        onClick={() => {
+                            setIsSignUpMode(!isSignUpMode);
+                        }}
+                    >
+                        {isSignUpMode ? '로그인' : '회원가입'}
+                    </span>
+                </div>
             </div>
         </Modal>
     );
