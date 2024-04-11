15bf13de21aaedf59c611765e384d88c466d2a89
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-11 16:05:13 +0900

    feat: Toolbar 아이콘 툴팁 추가

--- src/Pages/qnas/Components/TextEditor/Toolbar.tsx
+++ src/Pages/qnas/Components/TextEditor/Toolbar.tsx
@@ -13,14 +13,26 @@ export default function Toolbar() {
             style={{ borderColor: 'rgb(229 231 235)', border: 'none', display: 'flex', justifyContent: 'center' }}
         >
             <span className={'ql-formats !flex !gap-x-4'}>
-                <button className={'ql-bold !size-10'} />
-                <button className={'ql-italic !size-10'} />
-                <button className={'ql-underline !size-10'} />
-                <button className={'ql-strike !size-10'} />
+                <div className={'tooltip tooltip-bottom'} data-tip={'볼드체'}>
+                    <button className={'ql-bold !size-10'} />
+                </div>
+                <div className={'tooltip tooltip-bottom'} data-tip={'이탤릭체'}>
+                    <button className={'ql-italic !size-10'} />
+                </div>
+                <div className={'tooltip tooltip-bottom'} data-tip={'밑줄'}>
+                    <button className={'ql-underline !size-10'} />
+                </div>
+                <div className={'tooltip tooltip-bottom'} data-tip={'취소선'}>
+                    <button className={'ql-strike !size-10'} />
+                </div>
             </span>
             <span className={'ql-formats !flex !gap-x-4'}>
-                <button className={'ql-list !size-10'} value={'ordered'} />
-                <button className={'ql-list !size-10'} value={'bullet'} />
+                <div className={'tooltip tooltip-bottom'} data-tip={'숫자 리스트'}>
+                    <button className={'ql-list !size-10'} value={'ordered'} />
+                </div>
+                <div className={'tooltip tooltip-bottom'} data-tip={'불릿 리스트'}>
+                    <button className={'ql-list !size-10'} value={'bullet'} />
+                </div>
             </span>
         </div>
     );
