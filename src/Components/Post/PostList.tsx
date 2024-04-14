aca016e2fc42f44bb3a5f11e257736111147aeb0
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-14 16:13:39 +0900

    fix: keyword 키의 모든 값을 가져오도록 수정

--- src/Components/Post/PostList.tsx
+++ src/Components/Post/PostList.tsx
@@ -1,19 +1,29 @@
 import { useSearchParams } from 'react-router-dom';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import PostListItem from '@/Components/Post/PostListItem.tsx';
 import getPostList from '@/Utils/getPostList.ts';
 
 export default function PostList() {
     const [searchParams] = useSearchParams();
 
-    const postType = searchParams.get('post_type') ?? 'develop';
+    const postType = searchParams.get('post_type') ?? 'dev';
     const title = searchParams.get('post_title') ?? '';
-    const keyword = searchParams.get('keyword') ?? '';
+    const keywords = searchParams.getAll('keyword') ?? [];
 
-    const { data: postList } = useSuspenseQuery({
-        queryKey: ['post', { postType, title, keyword }],
+    const {
+        data: postList,
+        isLoading,
+        isPending,
+    } = useQuery({
+        queryKey: ['post', { postType, title, keywords }],
         queryFn: getPostList,
+        gcTime: 0,
+        throwOnError: true,
     });
+
+    if (isLoading || isPending) {
+        return <div>로딩</div>;
+    }
 
     return (
         <div className={'grid grid-cols-4 gap-x-3.5 gap-y-4'}>
