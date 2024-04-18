c894bde546325b5ae951a93b7c59f5e6949df9ce
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-18 20:30:15 +0900

    feat: 질문 게시글 작성, 수정 페이지에서는 GNB 가리도록 수정

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,19 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import GlobalNavbar from '@/Components/GNB/GlobalNavbar.tsx';
 
+const shouldHideGNB = (pathname: string): boolean => {
+    if (pathname.includes('/qnas/write') || pathname.includes('/qnas/edit')) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 export default function Layout() {
+    const { pathname } = useLocation();
+
     return (
         <div>
-            <header
-                className={
-                    'fixed z-50 flex h-[5rem] w-full flex-col justify-center border-b border-gray-300 bg-white/50 backdrop-blur-md'
-                }
-            >
-                <div className={'mx-40'}>
-                    <GlobalNavbar />
-                </div>
-            </header>
-            <div className={'pt-28'}>
+            {!shouldHideGNB(pathname) && (
+                <header
+                    className={
+                        'fixed top-0 z-50 flex h-[5rem] w-full flex-col justify-center border-b border-gray-300 bg-white/50 backdrop-blur-md'
+                    }
+                >
+                    <div className={'mx-40'}>
+                        <GlobalNavbar />
+                    </div>
+                </header>
+            )}
+            <div>
                 <Outlet />
             </div>
         </div>
