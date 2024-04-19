dedcd76e4629c71cd9063e89b565e2b69d2b3dfb
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-19 16:36:37 +0900

    refactor: 쿼리 파라미터 키 이름 수정
    post_type -> qna_type

--- src/Components/PostSearchFilter/PostTypeToggleButton.tsx
+++ src/Components/PostSearchFilter/PostTypeToggleButton.tsx
@@ -4,20 +4,20 @@ import { ComputerDesktopIcon, UserIcon } from '@heroicons/react/24/outline';
 export default function PostTypeToggleButton() {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const currentPostType = searchParams.get('post_type') ?? 'dev';
+    const currentQnAType = searchParams.get('qna_type') ?? 'dev';
 
-    const handleButtonClick = (targetPostType: string) => {
-        if (targetPostType === currentPostType) {
+    const handleButtonClick = (targetQnAType: string) => {
+        if (targetQnAType === currentQnAType) {
             return;
         }
-        searchParams.set('post_type', targetPostType);
+        searchParams.set('qna_type', targetQnAType);
         setSearchParams(searchParams);
     };
 
     return (
         <div className={'flex h-fit items-center justify-center rounded-lg border border-gray-300 p-1'}>
             <button
-                className={`flex cursor-pointer justify-center gap-x-1 rounded px-2 py-1 text-gray-700 transition-all ${currentPostType === 'dev' ? 'bg-gray-200 font-medium' : 'bg-white font-normal'}`}
+                className={`flex cursor-pointer justify-center gap-x-1 rounded px-2 py-1 text-gray-700 transition-all ${currentQnAType === 'dev' ? 'bg-gray-200 font-medium' : 'bg-white font-normal'}`}
                 type={'button'}
                 onClick={() => {
                     handleButtonClick('dev');
@@ -27,7 +27,7 @@ export default function PostTypeToggleButton() {
                 <span className={'text-[0.9rem]'}>개발</span>
             </button>
             <button
-                className={`flex cursor-pointer justify-center gap-x-1 rounded px-2 py-1 text-gray-700 transition-all ${currentPostType === 'nonDev' ? 'bg-gray-200 font-medium' : 'bg-white font-normal'}`}
+                className={`flex cursor-pointer justify-center gap-x-1 rounded px-2 py-1 text-gray-700 transition-all ${currentQnAType === 'nonDev' ? 'bg-gray-200 font-medium' : 'bg-white font-normal'}`}
                 type={'button'}
                 onClick={() => {
                     handleButtonClick('nonDev');
