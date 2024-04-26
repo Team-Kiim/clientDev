7e26d5dd1a4504bf70983cfe11e06b911afaca30
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-27 02:03:35 +0900

    design: 게시글 작성 페이지 디자인 수정

--- src/Pages/qnas/write/page.tsx
+++ src/Pages/qnas/write/page.tsx
@@ -3,9 +3,11 @@ import QnAWriteForm from '@/Pages/qnas/write/Components/QnAWriteForm.tsx';
 
 export default function Page() {
     return (
-        <div className={'mx-[29rem] my-3 flex w-[50rem] flex-col gap-y-3'}>
-            <QnATypeDecider />
-            <QnAWriteForm />
+        <div className={'flex w-full min-w-[1500px] justify-center'}>
+            <div className={'my-3 flex w-[50rem] flex-col gap-y-3'}>
+                <QnATypeDecider />
+                <QnAWriteForm />
+            </div>
         </div>
     );
 }
