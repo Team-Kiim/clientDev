8cf72e791e4380c16a3fa9e17b4c78c8b7ab92aa
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-28 16:28:15 +0900

    feat: 게시글 조회수 보여주는 UI 추가

--- src/Pages/qnas/[boardId]/Components/PostMetaInfo.tsx
+++ src/Pages/qnas/[boardId]/Components/PostMetaInfo.tsx
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko.js';
+import formatNumber from '@/Utils/formatNumber.ts';
+
 dayjs.locale('ko');
 
 interface Props {
@@ -7,9 +9,10 @@ interface Props {
     title: string;
     userNickname: string;
     profileImgSrc: string;
+    viewCount: number;
 }
 
-export default function PostMetaInfo({ createdTime, title, userNickname, profileImgSrc }: Props) {
+export default function PostMetaInfo({ createdTime, title, userNickname, profileImgSrc, viewCount }: Props) {
     const createdDate = new Date(createdTime[0], createdTime[1] - 1, createdTime[2]);
     const createdWeekDay = dayjs(createdDate).format('dddd');
 
@@ -19,13 +22,18 @@ export default function PostMetaInfo({ createdTime, title, userNickname, profile
                 {`${createdWeekDay}, ${createdTime[0]}년 ${createdTime[1]}월 ${createdTime[2]}일`}
             </span>
             <h1 className={'line-clamp-1 text-4xl font-bold'}>{title}</h1>
-            <div className={'flex items-center gap-x-3'}>
-                <div className={'avatar'}>
-                    <div className={'size-10 rounded-full'}>
-                        <img src={profileImgSrc} alt={'profileImage'} />
+            <div className={'flex items-center justify-between'}>
+                <div className={'flex items-center gap-x-3'}>
+                    <div className={'avatar'}>
+                        <div className={'size-10 rounded-full'}>
+                            <img src={profileImgSrc} alt={'profileImage'} />
+                        </div>
                     </div>
+                    <span className={'text-[0.95rem] tracking-wider text-gray-700'}>{userNickname}</span>
                 </div>
-                <span className={'text-[0.95rem] tracking-wider text-gray-700'}>{userNickname}</span>
+                <div className={'text-[0.9rem] text-gray-500'}>
+                    <span>조회수 : {formatNumber(viewCount, 0)}</span>
+                </div>
             </div>
         </section>
     );
