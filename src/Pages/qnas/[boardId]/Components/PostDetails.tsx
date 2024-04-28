f31d4b494ebbd6325b3fc2472e8a5ecd8c0d9168
Kim Kangmin <rlarkdals1202@gmail.com>
2024-04-28 16:29:53 +0900

    refactor: 게시글 관련 데이터 prop 으로 전달받도록 수정

--- src/Pages/qnas/[boardId]/Components/PostDetails.tsx
+++ src/Pages/qnas/[boardId]/Components/PostDetails.tsx
@@ -1,30 +1,32 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
 import PostMetaInfo from '@/Pages/qnas/[boardId]/Components/PostMetaInfo.tsx';
 import PostContent from '@/Pages/qnas/[boardId]/Components/PostContent.tsx';
-import getSinglePostData from '@/Pages/qnas/[boardId]/Utils/getSinglePostData.ts';
+import type { Post } from '@/Types/PostTypes.ts';
 
-export default function PostDetails() {
-    const postId = useParams().postId;
+type Props = Omit<Post, 'isBookmarked' | 'likeCount' | 'dislikeCount'>;
 
-    const { data } = useSuspenseQuery({
-        queryKey: ['post', postId],
-        queryFn: getSinglePostData,
-    });
-
-    console.log(data);
-
+export default function PostDetails({
+    id,
+    title,
+    userNickname,
+    profileImgSrc,
+    bodyContent,
+    viewCount,
+    createdTime,
+    modifiedTime,
+    skillCategories,
+}: Props) {
     return (
         <>
-            <main className={'col-span-7 flex h-[300px] max-w-full flex-col gap-y-5'}>
+            <main className={'flex max-w-full flex-col gap-y-5'}>
                 <PostMetaInfo
-                    createdTime={data.createdTime}
-                    title={data.title}
-                    userNickname={data.userNickname}
-                    profileImgSrc={data.profileImgSrc}
+                    createdTime={createdTime}
+                    title={title}
+                    userNickname={userNickname}
+                    profileImgSrc={profileImgSrc}
+                    viewCount={viewCount}
                 />
                 <hr />
-                <PostContent bodyContent={data.bodyContent} />
+                <PostContent bodyContent={bodyContent} />
             </main>
         </>
     );
