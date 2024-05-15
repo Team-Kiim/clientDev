63c3be56cd9c7b6e3b7b7ac69c37e2eaeb31bf54
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-15 23:27:43 +0900

    design: QnA 유형 토글 버튼 디자인 변경

--- src/Components/PostSearchFilter/QnATypeToggleButtons.tsx
+++ src/Components/PostSearchFilter/QnATypeToggleButtons.tsx
@@ -15,26 +15,26 @@ export default function QnATypeToggleButtons() {
     };
 
     return (
-        <div className={'flex h-fit items-center justify-center rounded-lg border border-gray-300 p-1'}>
+        <div className={'flex items-center gap-x-2'}>
             <button
-                className={`flex cursor-pointer justify-center gap-x-1 rounded px-2 py-1 text-gray-700 transition-all ${currentQnAType === 'dev' ? 'bg-gray-200 font-medium' : 'bg-white font-normal'}`}
+                className={`${currentQnAType === 'dev' ? 'border-violet-600 bg-violet-600 text-white' : 'border-gray-300 bg-white text-black'} flex items-center gap-x-1.5 rounded-xl border px-3.5 py-2`}
                 type={'button'}
                 onClick={() => {
                     handleButtonClick('dev');
                 }}
             >
                 <ComputerDesktopIcon className={'size-5'} />
-                <span className={'text-[0.9rem]'}>개발</span>
+                <span className={'text-[0.87rem] font-bold'}>개발</span>
             </button>
             <button
-                className={`flex cursor-pointer justify-center gap-x-1 rounded px-2 py-1 text-gray-700 transition-all ${currentQnAType === 'nonDev' ? 'bg-gray-200 font-medium' : 'bg-white font-normal'}`}
+                className={`${currentQnAType === 'nonDev' ? 'border-violet-600 bg-violet-600 text-white' : 'border-gray-300 bg-white text-black'} flex items-center gap-x-1.5 rounded-xl border border-gray-300 px-3.5 py-2`}
                 type={'button'}
                 onClick={() => {
                     handleButtonClick('nonDev');
                 }}
             >
                 <UserIcon className={'size-5'} />
-                <span className={'text-[0.9rem]'}>비개발</span>
+                <span className={'text-[0.87rem] font-bold'}>비개발</span>
             </button>
         </div>
     );
