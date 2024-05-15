c622e588db4ec2cda5b22bbc7a3fd31287486c7c
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-15 23:28:43 +0900

    design: margin-top 값 수정

--- src/Pages/page.tsx
+++ src/Pages/page.tsx
@@ -4,10 +4,10 @@ import PostAddButton from '@/Components/Post/PostAddButton.tsx';
 
 export default function Page() {
     return (
-        <div className={'mt-12 flex w-full min-w-[1500px] justify-center'}>
+        <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
             <div className={'w-[87.5rem]'}>
                 <SearchFilter />
-                <main className={'my-12'}>
+                <main className={'my-8'}>
                     <PostList />
                 </main>
                 <PostAddButton />
