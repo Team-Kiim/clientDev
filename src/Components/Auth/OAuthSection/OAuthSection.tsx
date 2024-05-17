bbd68c4add09dd54721dfbed2d4a0f4e4e783bd7
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-17 23:09:14 +0900

    design: 버튼 간격 수정

--- src/Components/Auth/OAuthSection/OAuthSection.tsx
+++ src/Components/Auth/OAuthSection/OAuthSection.tsx
@@ -1,10 +1,10 @@
-import KakaoSection from '@/Components/Auth/OAuthSection/KakaoSection.tsx';
+import KakaoSection from '@/Components/Auth/OAuthSection/KakaoSection/KakaoSection.tsx';
 import NaverSection from '@/Components/Auth/OAuthSection/NaverSection/NaverSection.tsx';
 import GoogleSection from '@/Components/Auth/OAuthSection/GoogleSection.tsx';
 
 export default function OAuthSection() {
     return (
-        <div className={'flex w-full items-center justify-center gap-x-4'}>
+        <div className={'flex w-full items-center justify-center gap-x-9'}>
             <KakaoSection />
             <NaverSection />
             <GoogleSection />
