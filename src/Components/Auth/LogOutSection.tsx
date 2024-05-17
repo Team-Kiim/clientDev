c52496aa5b563c21c7640b889a5f3ddbf6a7659f
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-17 23:09:00 +0900

    fix: 로그인, 회원가입 버튼 누르면 맞는 페이지에 가도록 수정

--- src/Components/Auth/LogOutSection.tsx
+++ src/Components/Auth/LogOutSection.tsx
@@ -1,29 +1,16 @@
-import { useState } from 'react';
-import AuthModal from '@/Components/Auth/AuthModal.tsx';
+import { Link } from 'react-router-dom';
 
 export default function LogOutSection() {
-    const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
-
     return (
         <>
-            <div className={'justify-self-end'}>
-                <button
-                    className={
-                        'rounded-lg bg-violet-600 px-3.5 py-2.5 text-[0.9rem] font-semibold text-white transition-all hover:bg-violet-700'
-                    }
-                    onClick={() => {
-                        setIsAuthModalOpen(true);
-                    }}
-                >
+            <div className={'flex gap-x-6 justify-self-end'}>
+                <Link className={'font-bold transition-all hover:text-violet-700'} to={'/sign_in'}>
                     로그인
-                </button>
+                </Link>
+                <Link className={'font-bold transition-all hover:text-violet-700'} to={'/sign_up'}>
+                    회원가입
+                </Link>
             </div>
-            <AuthModal
-                isAuthModalOpen={isAuthModalOpen}
-                closeModal={() => {
-                    setIsAuthModalOpen(false);
-                }}
-            />
         </>
     );
 }
