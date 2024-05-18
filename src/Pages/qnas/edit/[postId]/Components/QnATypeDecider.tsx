a938d2e7615a11913f0f19232df2aa66eb78ee44
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-19 05:59:19 +0900

    design: 질문 유형 결정하는 부분 디자인 변경

--- src/Pages/qnas/edit/[postId]/Components/QnATypeDecider.tsx
+++ src/Pages/qnas/edit/[postId]/Components/QnATypeDecider.tsx
@@ -10,13 +10,13 @@ export default function QnATypeDecider() {
     return (
         <div className={'flex items-center gap-x-5 border-b border-gray-300 pb-2'}>
             <button
-                className={'rounded-lg px-2 py-1 transition-all hover:bg-violet-50'}
+                className={'px-2 py-1'}
                 type={'button'}
                 onClick={() => {
                     handleQnATypeChangeButtonClick('dev');
                 }}
             >
-                <span className={`${qnaType === 'dev' ? 'text-violet-600' : 'text-gray-600'} font-medium`}>개발</span>
+                <span className={`${qnaType === 'dev' ? 'text-violet-700' : 'text-gray-500'} font-bold`}>개발</span>
             </button>
             <button
                 className={'rounded-lg px-2 py-1 transition-all hover:bg-violet-50'}
@@ -25,7 +25,7 @@ export default function QnATypeDecider() {
                     handleQnATypeChangeButtonClick('nonDev');
                 }}
             >
-                <span className={`${qnaType === 'nonDev' ? 'text-violet-600' : 'text-gray-600'} font-medium`}>
+                <span className={`${qnaType === 'nonDev' ? 'text-violet-700' : 'text-gray-500'} font-bold`}>
                     비개발
                 </span>
             </button>
