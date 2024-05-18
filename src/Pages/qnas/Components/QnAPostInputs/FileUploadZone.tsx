d6bba53ccb7d95ed174b7c7bdf285f2f73f1bfa9
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-19 06:01:02 +0900

    design: 파일 업로드 영역 디자인 변경

--- src/Pages/qnas/Components/QnAPostInputs/FileUploadZone.tsx
+++ src/Pages/qnas/Components/QnAPostInputs/FileUploadZone.tsx
@@ -20,18 +20,18 @@ export default function FileUploadZone({ uploadFiles }: Props) {
             {...getRootProps()}
         >
             <input {...getInputProps()} />
-            <div className={'flex w-full flex-col items-center justify-center gap-y-2'}>
+            <div className={'flex w-full flex-col items-center justify-center gap-y-2.5'}>
                 <div className={'rounded-full bg-violet-50 p-1.5'}>
                     <FiUpload className={'size-7 text-violet-600'} />
                 </div>
-                <span className={'text-[0.95rem] font-medium'}>드로그 앤 드롭으로 파일을 추가</span>
+                <span className={'text-[0.9rem] font-bold'}>드로그 앤 드롭으로 파일을 추가</span>
                 <span className={'text-sm text-gray-600'}>또는</span>
                 <button
                     className={'rounded-lg px-2 py-1 transition-all hover:bg-violet-50'}
                     type={'button'}
                     onClick={open}
                 >
-                    <span className={'text-[0.95rem] font-medium text-violet-600'}>파일 탐색</span>
+                    <span className={'text-[0.9rem] font-bold text-violet-700'}>파일 탐색</span>
                 </button>
             </div>
         </div>
