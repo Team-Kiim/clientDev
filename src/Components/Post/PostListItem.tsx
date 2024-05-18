a2bd869e808922b57bb5ebaf1a0ac483e9f9f05d
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-18 17:52:41 +0900

    design: 게시글 리스트 아이템 디자인 변경

--- src/Components/Post/PostListItem.tsx
+++ src/Components/Post/PostListItem.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
-import { BiLike } from 'react-icons/bi';
-import { CalendarIcon, EyeIcon } from '@heroicons/react/24/outline';
+import { CalendarIcon } from '@heroicons/react/24/outline';
+import { BiSolidLike } from 'react-icons/bi';
+import { TiEye } from 'react-icons/ti';
 import formatNumber from '@/Utils/formatNumber.ts';
 import type { Post } from '@/Types/PostTypes.ts';
 
@@ -12,22 +13,22 @@ export default function PostListItem({ post }: Props) {
     const { id, title, userNickname, profileImgSrc, bodyContent, viewCount, likeCount, createdTime } = post;
 
     return (
-        <li>
+        <li
+            className={
+                'h-[15.5rem] rounded-md border border-gray-100 shadow-lg transition-all hover:-translate-y-2 hover:shadow-xl'
+            }
+        >
             <Link to={`/qnas/${id}`}>
-                <div
-                    className={
-                        'flex h-64 flex-col rounded-md border border-gray-200 shadow-md transition-all hover:-translate-y-2 hover:shadow-lg'
-                    }
-                >
-                    <div className={'flex items-center gap-x-2 px-3.5 py-2 text-gray-500'}>
+                <div className={'flex h-full flex-col'}>
+                    <div className={'flex items-center gap-x-2.5 px-3.5 py-2 text-gray-600'}>
                         <CalendarIcon className={'size-4'} />
                         <span
-                            className={'text-[0.82rem]'}
+                            className={'text-[0.8rem]'}
                         >{`${createdTime[0]}년 ${createdTime[1]}월 ${createdTime[2]}일`}</span>
                     </div>
                     <div className={'flex flex-1 flex-col gap-y-3 px-3.5'}>
-                        <h1 className={'line-clamp-1 text-xl font-medium'}>{title}</h1>
-                        <p className={'line-clamp-3 text-sm text-gray-500'}>{bodyContent.replace(/<[^>]+>/g, '')}</p>
+                        <h1 className={'line-clamp-1 text-lg font-bold'}>{title}</h1>
+                        <p className={'line-clamp-4 text-sm text-gray-500'}>{bodyContent.replace(/<[^>]+>/g, '')}</p>
                     </div>
                     <div
                         className={
@@ -38,16 +39,16 @@ export default function PostListItem({ post }: Props) {
                             <div className={'avatar size-7'}>
                                 <img className={'rounded-full'} src={profileImgSrc} alt={'profileImgExample'} />
                             </div>
-                            <span className={'text-[0.82rem] font-medium'}>{userNickname}</span>
+                            <span className={'text-[0.82rem] font-bold'}>{userNickname}</span>
                         </div>
                         <div className={'flex items-center gap-x-4'}>
                             <div className={'flex items-center gap-x-1'}>
-                                <BiLike className={'size-4 text-blue-600'} />
-                                <span className={'text-[0.8rem]'}>{formatNumber(likeCount, 0)}</span>
+                                <BiSolidLike className={'size-4 text-gray-700'} />
+                                <span className={'text-[0.8rem] font-bold'}>{formatNumber(likeCount, 0)}</span>
                             </div>
                             <div className={'flex items-center gap-x-1'}>
-                                <EyeIcon className={'size-5 text-violet-600'} />
-                                <span className={'text-[0.8rem]'}>{formatNumber(viewCount, 0)}</span>
+                                <TiEye className={'size-5 text-gray-700'} />
+                                <span className={'text-[0.8rem] font-bold'}>{formatNumber(viewCount, 0)}</span>
                             </div>
                         </div>
                     </div>
