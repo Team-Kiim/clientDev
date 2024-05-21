2262588dc4a108e2e9c38e127d3ee31d556aed36
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-21 16:33:52 +0900

    design: 좋아요 버튼 디자인 변경

--- src/Pages/qnas/[boardId]/Components/LikeButtonWithCount.tsx
+++ src/Pages/qnas/[boardId]/Components/LikeButtonWithCount.tsx
@@ -1,7 +1,7 @@
 import { debounce } from 'lodash';
 import { useCallback, useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { BiLike } from 'react-icons/bi';
+import { BiLike, BiSolidLike } from 'react-icons/bi';
 import type { Post } from '@/Types/PostTypes.ts';
 import formatNumber from '@/Utils/formatNumber.ts';
 
@@ -67,17 +67,18 @@ export default function LikeButtonWithCount({ isMemberLiked, likeCount, postId }
     };
 
     return (
-        <div className={'flex flex-col items-center gap-y-2'}>
-            <div className={'tooltip tooltip-left'} data-tip={isMemberLiked ? '좋아요 취소' : '좋아요'}>
-                <button
-                    className={`rounded-full border p-2.5 transition-all active:scale-95 ${isMemberLiked ? 'border-blue-200 bg-blue-50 text-blue-600' : 'border-gray-200 bg-white text-gray-700'} shadow-md`}
-                    type={'button'}
-                    onClick={handleLikeButtonClick}
-                >
-                    <BiLike className={'size-7'} />
-                </button>
+        <div className={'flex items-center gap-x-2'}>
+            <div className={'text-[0.9rem] text-gray-600'}>
+                {'좋아요 '}
+                <span className={'font-bold'}>{formatNumber(likeCount, 0)}</span>
             </div>
-            <span className={'text-[0.95rem] font-medium text-gray-500'}>{formatNumber(likeCount, 0)}</span>
+            <button
+                className={'text-gray-700 transition-all active:scale-95'}
+                type={'button'}
+                onClick={handleLikeButtonClick}
+            >
+                {isMemberLiked ? <BiSolidLike className={'size-7'} /> : <BiLike className={'size-7'} />}
+            </button>
         </div>
     );
 }
