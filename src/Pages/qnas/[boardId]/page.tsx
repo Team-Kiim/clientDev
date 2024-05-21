e2005acf32e5b6b06d3cd4cb78f5dc47c691c505
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-21 17:00:25 +0900

    design: div 요소의 너비를 70rem에서 64rem으로 변경

--- src/Pages/qnas/[boardId]/page.tsx
+++ src/Pages/qnas/[boardId]/page.tsx
@@ -4,7 +4,7 @@ import PostView from '@/Pages/qnas/[boardId]/Components/PostView.tsx';
 export default function Page() {
     return (
         <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
-            <div className={'grid w-[70rem] grid-cols-10 gap-x-7'}>
+            <div className={'grid w-[64rem] grid-cols-10 gap-x-7'}>
                 <Suspense>
                     <PostView />
                 </Suspense>
