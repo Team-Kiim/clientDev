c188ab2334052ecc1b966a2b014fe694b89845ee
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-25 03:54:29 +0900

    design: 로그인 폼 디자인 변경

--- src/Pages/sign_in/Component/SignInForm.tsx
+++ src/Pages/sign_in/Component/SignInForm.tsx
@@ -31,36 +31,48 @@ export default function SignInForm() {
     return (
         <form className={'flex w-full flex-col gap-y-8'} onSubmit={handleSubmit(onSubmit)}>
             <div className={'flex w-full flex-col gap-y-7'}>
-                <div className={'flex w-full flex-col gap-y-1.5'}>
-                    <input
-                        className={
-                            'w-full rounded-lg border border-gray-300 px-3.5 py-3 text-[0.93rem] focus:outline-violet-700'
-                        }
-                        type={'text'}
-                        placeholder={'이메일'}
-                        autoComplete={'off'}
-                        autoCapitalize={'off'}
-                        {...register('email', {
-                            required: { value: true, message: '이메일을 입력해주세요.' },
-                        })}
-                    />
-                    {errors?.email?.message && errors?.email.type === 'required' && (
-                        <div className={'m-0.5 flex items-center gap-x-1 text-red-700'}>
-                            <ExclamationTriangleIcon className={'size-5'} />
-                            <p className={'text-sm'}>{errors.email.message}</p>
-                        </div>
-                    )}
-                </div>
-                <div className={'flex w-full flex-col gap-y-1.5'}>
+                <div className={'flex w-full flex-col gap-y-2'}>
+                    <label className={'mx-2 w-fit text-[0.9rem] font-bold'} htmlFor={'emailInput'}>
+                        이메일
+                    </label>
                     <div
                         className={
-                            'flex rounded-lg border border-gray-300 px-3.5 py-3 text-[0.93rem] focus-within:border-white focus-within:outline focus-within:outline-2 focus-within:outline-offset-[-2px] focus-within:outline-violet-700'
+                            'flex items-center rounded-md border border-gray-300 px-3.5 py-3 focus-within:border-violet-700'
                         }
                     >
                         <input
-                            className={'w-full flex-1 focus:outline-none'}
+                            id={'emailInput'}
+                            className={'w-full text-[0.9rem] focus:outline-none'}
+                            type={'text'}
+                            placeholder={'가입한 이메일 주소 입력'}
+                            autoComplete={'off'}
+                            autoCapitalize={'off'}
+                            {...register('email', {
+                                required: { value: true, message: '이메일을 입력해주세요.' },
+                            })}
+                        />
+                    </div>
+                    {errors?.email?.message && errors?.email.type === 'required' && (
+                        <div className={'m-0.5 flex items-center gap-x-1 text-red-700'}>
+                            <ExclamationTriangleIcon className={'size-5'} />
+                            <p className={'text-[0.8rem] font-bold'}>{errors.email.message}</p>
+                        </div>
+                    )}
+                </div>
+                <div className={'flex w-full flex-col gap-y-2'}>
+                    <label className={'mx-2 w-fit text-[0.9rem] font-bold'} htmlFor={'passwordInput'}>
+                        비밀번호
+                    </label>
+                    <div
+                        className={
+                            'flex items-center rounded-md border border-gray-300 px-3.5 py-3 focus-within:border-violet-700'
+                        }
+                    >
+                        <input
+                            id={'passwordInput'}
+                            className={'w-full flex-1 text-[0.9rem] focus:outline-none'}
                             type={isPasswordVisible ? 'text' : 'password'}
-                            placeholder={'비밀번호'}
+                            placeholder={'비밀번호 입력'}
                             autoComplete={'off'}
                             autoCapitalize={'off'}
                             {...register('password', {
@@ -77,16 +89,16 @@ export default function SignInForm() {
                             }}
                         >
                             {isPasswordVisible ? (
-                                <EyeSlashIcon className={'size-5'} />
+                                <EyeSlashIcon className={'size-5 scale-110'} />
                             ) : (
-                                <EyeIcon className={'size-5'} />
+                                <EyeIcon className={'size-5 scale-110'} />
                             )}
                         </button>
                     </div>
                     {errors?.password?.message && errors?.password.type === 'required' && (
                         <div className={'m-0.5 flex items-center gap-x-1 text-red-700'}>
                             <ExclamationTriangleIcon className={'size-5'} />
-                            <p className={'text-sm'}>{errors.password.message}</p>
+                            <p className={'text-[0.8rem] font-bold'}>{errors.password.message}</p>
                         </div>
                     )}
                 </div>
