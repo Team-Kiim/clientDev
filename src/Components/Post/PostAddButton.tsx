0e83884b579d3235215d581eb763f4065eb6dd0d
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-25 18:54:37 +0900

    design: 게시글 작성 버튼 디자인 변경

--- src/Components/Post/PostAddButton.tsx
+++ src/Components/Post/PostAddButton.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { PlusIcon } from '@heroicons/react/24/outline';
+import { GoPencil } from 'react-icons/go';
 
 export default function PostAddButton() {
     const navigate = useNavigate();
@@ -9,16 +9,17 @@ export default function PostAddButton() {
     };
 
     return (
-        <div className={'tooltip tooltip-top fixed bottom-10 right-40'} data-tip={'Q&A 작성'}>
-            <button
-                className={'rounded-full bg-violet-600 p-2'}
-                onClick={() => {
-                    handlePostAddButtonClick();
-                }}
-                type={'button'}
-            >
-                <PlusIcon className={'size-11 text-white'} />
-            </button>
-        </div>
+        <button
+            className={
+                'flex items-center gap-x-2 rounded-xl bg-violet-600 px-3.5 py-2.5 text-white transition-all hover:bg-violet-700'
+            }
+            onClick={() => {
+                handlePostAddButtonClick();
+            }}
+            type={'button'}
+        >
+            <GoPencil className={'size-5'} />
+            <span className={'text-[0.9rem] font-bold'}>글작성</span>
+        </button>
     );
 }
