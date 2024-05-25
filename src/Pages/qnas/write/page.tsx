9db927121fcd312d4c1b97bbc197282042ae76d7
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-26 04:36:18 +0900

    design: QnA 게시글 작성 페이지 너비 변경

--- src/Pages/qnas/write/page.tsx
+++ src/Pages/qnas/write/page.tsx
@@ -3,7 +3,7 @@ import QnAWriteForm from '@/Pages/qnas/write/Components/QnAWriteForm.tsx';
 export default function Page() {
     return (
         <div className={'flex w-full min-w-[1500px] justify-center'}>
-            <div className={'my-5 flex w-[50rem] flex-col gap-y-3'}>
+            <div className={'my-5 flex w-[45rem] flex-col gap-y-3'}>
                 <h1 className={'text-2xl font-extrabold'}>QnA 게시글 작성</h1>
                 <QnAWriteForm />
             </div>
