f4f2b20f39a54fffa492e39b9070edbf00b8c477
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-25 19:47:36 +0900

    refactor: QnA 유형 결정하는 기능 제거
    게시글 작성 페이지 분리(QnA, 커뮤니티)로 해당 기능이 더 이상 필요하지 않게 됨.

--- src/Pages/qnas/write/page.tsx
+++ src/Pages/qnas/write/page.tsx
@@ -1,11 +1,9 @@
-import QnATypeDecider from '@/Pages/qnas/write/Components/QnATypeDecider.tsx';
 import QnAWriteForm from '@/Pages/qnas/write/Components/QnAWriteForm.tsx';
 
 export default function Page() {
     return (
         <div className={'flex w-full min-w-[1500px] justify-center'}>
             <div className={'my-3 flex w-[50rem] flex-col gap-y-3'}>
-                <QnATypeDecider />
                 <QnAWriteForm />
             </div>
         </div>
