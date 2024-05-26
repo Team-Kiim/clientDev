8ed9a3c45e6a50834513b00db54741a8bef49376
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-27 03:32:14 +0900

    feat: 카카오 소셜 로그인 기능 추가

---
+++ src/Components/Auth/OAuthSection/KakaoSection/KakaoRedirect.tsx
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, useSearchParams } from 'react-router-dom';
+
+export default function KakaoRedirect() {
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get('code');
+    const { VITE_DEV_URL, VITE_KAKAO_CLIENT_SECRET, VITE_KAKAO_REST_API_KEY } = import.meta.env;
+
+    const { isSuccess, isError } = useQuery({
+        queryKey: ['oauth', 'kakao'],
+        queryFn: () => {
+            return axios
+                .post('/api/oauth/get-kakao-code', {
+                    clientId: VITE_KAKAO_REST_API_KEY,
+                    redirectUri: `${VITE_DEV_URL}/oauth/kakao`,
+                    code: code,
+                    clientSecret: VITE_KAKAO_CLIENT_SECRET,
+                })
+                .then(response => {
+                    return response.data.access_token;
+                })
+                .then((accessToken: string) => {
+                    return axios
+                        .post(`/api/oauth/get-member-info`, {
+                            accessToken,
+                            socialLoginType: 'KAKAO',
+                        })
+                        .then(response => {
+                            const kakaoAccount = response.data['kakao_account'];
+                            const profile = response.data.properties;
+                            const email = kakaoAccount.email;
+                            const nickname = profile.nickname;
+                            return axios
+                                .post('/api/oauth/login', {
+                                    email,
+                                    nickname,
+                                })
+                                .then(response => {
+                                    console.log(response);
+                                    return true;
+                                });
+                        });
+                });
+        },
+        gcTime: 0,
+    });
+
+    if (isSuccess) {
+        return <Navigate to={'/'} replace={true} />;
+    }
+
+    if (isError) {
+        window.alert('로그인에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+        return <Navigate to={'/'} replace={true} />;
+    }
+
+    return null;
+}
