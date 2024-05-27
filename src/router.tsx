ce3dcd25f6772f86c000b61aa714964b4dae80c5
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-27 18:35:02 +0900

    feat: 커뮤니티 게시글 목록 페이지 관련 라우트 컴포넌트 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import AppLayout from '@/layout.tsx';
 import MainPage from '@/Pages/page.tsx';
+import CommunityPostListPage from '@/Pages/community/page.tsx';
 import SignInPage from '@/Pages/sign_in/page.tsx';
 import SignUpPage from '@/Pages/sign_up/page.tsx';
 import NaverRedirect from '@/Components/Auth/OAuthSection/NaverSection/NaverRedirect.tsx';
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
         <Route element={<AppLayout />}>
             <Route>
                 <Route element={<MainPage />} path={'/'} />
+                <Route element={<CommunityPostListPage />} path={'/community'} />
                 <Route element={<SignInPage />} path={'/sign_in'} />
                 <Route element={<SignUpPage />} path={'/sign_up'} />
                 <Route element={<NaverRedirect />} path={'/oauth/naver'} />
