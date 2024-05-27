2f37bd857a2f55f53fc3ef81ee1ade885afbd413
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-28 02:56:24 +0900

    fix: 단일 QnA 게시글 정보 가져오는 함수 변경

--- src/Pages/qnas/[boardId]/Components/PostView.tsx
+++ src/Pages/qnas/[boardId]/Components/PostView.tsx
@@ -1,16 +1,16 @@
 import { omit, pick } from 'lodash';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import PostDetails from '@/Pages/qnas/[boardId]/Components/PostDetails.tsx';
-import PostInteraction from '@/Pages/qnas/[boardId]/Components/PostInteraction.tsx';
-import getSinglePostData from '@/Pages/qnas/Utils/getSinglePostData.ts';
+import PostDetails from '@/Pages/Components/PostView/PostDetails.tsx';
+import PostInteraction from '@/Pages/Components/PostView/PostInteraction.tsx';
+import getSingleQnAPostInfo from '@/Pages/qnas/Utils/getSingleQnAPostInfo.ts';
 
 export default function PostView() {
     const postId = useParams().postId;
 
     const { data } = useSuspenseQuery({
         queryKey: ['post', postId],
-        queryFn: getSinglePostData,
+        queryFn: getSingleQnAPostInfo,
     });
 
     return (
