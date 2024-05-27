4b6da4e5c9986408453c0526f47a3d23ca6bba53
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-28 03:23:43 +0900

    feat: 단일 QnA 게시글 렌더링하는 컴포넌트 추가

---
+++ src/Pages/qnas/[boardId]/Components/PostDetails.tsx
@@ -0,0 +1,34 @@
+import PostMetaInfo from '@/Pages/Components/PostView/PostMetaInfo.tsx';
+import PostContent from '@/Pages/Components/PostView/PostContent.tsx';
+import PostControl from '@/Pages/Components/PostView/PostControl.tsx';
+import PostKeywordList from '@/Pages/qnas/[boardId]/Components/PostKeywordList.tsx';
+import type { QnAPostInfo } from '@/Types/PostInfo.ts';
+
+type Props = Omit<QnAPostInfo, 'isMemberLiked' | 'likeCount' | 'isMemberBookmarked' | 'imageFileInfoDtoList'>;
+
+export default function PostDetails({
+    id,
+    title,
+    nickname,
+    profileImagePath,
+    bodyContent,
+    viewCount,
+    createdTime,
+    skillCategories,
+}: Props) {
+    return (
+        <article className={'flex max-w-full flex-col gap-y-2.5'}>
+            <PostMetaInfo
+                createdTime={createdTime}
+                title={title}
+                nickname={nickname}
+                profileImgPath={profileImagePath}
+                viewCount={viewCount}
+            />
+            <PostControl postId={id} />
+            <hr />
+            <PostContent bodyContent={bodyContent} />
+            <PostKeywordList categories={skillCategories} />
+        </article>
+    );
+}
