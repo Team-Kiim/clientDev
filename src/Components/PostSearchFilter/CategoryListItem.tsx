53fcc8fbf9b3d019519a138c664009cd6985e041
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-29 20:23:38 +0900

    feat: 카테고리를 사용한 게시글 필터링 기능 추가

---
+++ src/Components/PostSearchFilter/CategoryListItem.tsx
@@ -0,0 +1,33 @@
+interface Props {
+    parentCategory: string;
+    childCategory: {
+        value: string;
+        label: string;
+    };
+    selectedCategories: {
+        parentCategory: string;
+        childCategory: string;
+    }[];
+    updateSelectedCategories(newCategory: { parentCategory: string; childCategory: string }): void;
+}
+
+export default function CategoryListItem({
+    parentCategory,
+    childCategory,
+    selectedCategories,
+    updateSelectedCategories,
+}: Props) {
+    return (
+        <li
+            className={`my-1.5 cursor-pointer rounded-2xl bg-gray-100 px-2.5 py-1.5 text-[0.8rem] font-bold ${selectedCategories.find(category => category.parentCategory === parentCategory && category.childCategory === childCategory.value) ? 'bg-violet-600 text-white' : 'bg-gray-100 text-black'} transition-all`}
+            onClick={() => {
+                updateSelectedCategories({
+                    parentCategory,
+                    childCategory: childCategory.value,
+                });
+            }}
+        >
+            {childCategory.label}
+        </li>
+    );
+}
--- src/Components/PostSearchFilter/SearchFilter.tsx
+++ src/Components/PostSearchFilter/SearchFilter.tsx
@@ -1,11 +1,42 @@
-import KeywordSearchInput from '@/Components/PostSearchFilter/KeywordSearchInput.tsx';
-import PostTypeToggleButton from '@/Components/PostSearchFilter/PostTypeToggleButton.tsx';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { HashtagIcon } from '@heroicons/react/24/outline';
+import CategorySelectDropdown from '@/Components/PostSearchFilter/CategorySelectDropdown.tsx';
 
 export default function SearchFilter() {
+    const [isCategorySelectDropdownOpen, setIsCategorySelectDropdownOpen] = useState(false);
+
+    const [searchParams] = useSearchParams();
+    const numberOfCurrentCategories = searchParams.getAll('category').length;
+
+    const closeCategorySelectDropdown = () => {
+        setIsCategorySelectDropdownOpen(false);
+    };
+
     return (
-        <div className={'flex justify-between'}>
-            <KeywordSearchInput />
-            <PostTypeToggleButton />
+        <div className={'relative flex items-center gap-x-5'}>
+            <button
+                className={`flex gap-x-2 rounded-2xl border px-3.5 py-2.5 transition-all ${numberOfCurrentCategories !== 0 ? 'border-violet-600 bg-violet-600 text-white hover:bg-violet-700' : 'border-gray-300 bg-white text-black hover:bg-gray-50'}`}
+                type={'button'}
+                onClick={event => {
+                    event.stopPropagation();
+                    setIsCategorySelectDropdownOpen(!isCategorySelectDropdownOpen);
+                }}
+            >
+                <HashtagIcon className={'size-5'} />
+                <div className={'flex gap-x-1.5 text-[0.9rem] font-bold'}>
+                    <span>카테고리</span>
+                    <span className={`${numberOfCurrentCategories !== 0 ? 'text-white' : 'text-violet-700'}`}>
+                        {numberOfCurrentCategories}
+                    </span>
+                </div>
+            </button>
+            {isCategorySelectDropdownOpen && (
+                <CategorySelectDropdown
+                    isDropdownOpen={isCategorySelectDropdownOpen}
+                    closeCategorySelectDropdown={closeCategorySelectDropdown}
+                />
+            )}
         </div>
     );
 }
