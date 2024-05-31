0941be8afcf6bd939c30c032ea3da6c945cbfe21
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-31 22:44:47 +0900

    design: margin 값 수정

--- src/Pages/Components/PostView/PostInteraction.tsx
+++ src/Pages/Components/PostView/PostInteraction.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default function PostInteraction({ isMemberLiked, likeCount, isMemberBookmarked, postId }: Props) {
     return (
-        <section className={'mb-2.5 flex items-center justify-end gap-x-4 border-b border-gray-300 pb-2.5'}>
+        <section className={'my-7 flex items-center justify-end gap-x-4 border-b border-gray-300 pb-2.5'}>
             <LikeButtonWithCount isMemberLiked={isMemberLiked} likeCount={likeCount} postId={postId} />
             <PostBookmarkButton isBookmarked={isMemberBookmarked} postId={postId} />
         </section>
