80c4fb124f7268139cc0fbcb9d906db94489bf30
Kim Kangmin <rlarkdals1202@gmail.com>
2024-06-14 22:27:21 +0900

    feat: IP 주소인지 확인하는 함수 추가

---
+++ src/Utils/checkIpAddress.ts
@@ -0,0 +1,8 @@
+const checkIpAddress = (domain: string): boolean => {
+    const regexForTest =
+        /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+    return regexForTest.test(domain);
+};
+
+export default checkIpAddress;
