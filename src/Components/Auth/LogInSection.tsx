7761e4828393e3f7487c2122752a0a805d66b50c
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-05 21:19:13 +0900

    refactor: 프로필 이미지 경로 수정

--- src/Components/Auth/LogInSection.tsx
+++ src/Components/Auth/LogInSection.tsx
@@ -1,8 +1,14 @@
-import { faker } from '@faker-js/faker';
 import { Link } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
 import { BellIcon, ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/outline';
+import type { User } from '@/Types/User.ts';
 
 export default function LogInSection() {
+    const queryClient = useQueryClient();
+
+    const { VITE_SERVER_URL } = import.meta.env;
+    const { profileImageName, profileImagePath } = queryClient.getQueryData<User>(['loggedIn user']);
+
     return (
         <div>
             <div className={'flex items-center gap-x-6 justify-self-end'}>
@@ -12,10 +18,13 @@ export default function LogInSection() {
                 <Link className={'rounded-full p-1 transition-all hover:bg-gray-100'} to={'/chat'}>
                     <ChatBubbleOvalLeftEllipsisIcon className={'size-7'} />
                 </Link>
-                <Link to={`/user/nickname`}>
+                <Link to={`/user`}>
                     <div className={'avatar flex items-center'}>
                         <div className={'size-7 rounded-full'}>
-                            <img src={faker.image.avatar()} alt={'example_profile_img'} />
+                            <img
+                                src={`${VITE_SERVER_URL}/image/${profileImagePath}/${profileImageName}`}
+                                alt={'example_profile_img'}
+                            />
                         </div>
                     </div>
                 </Link>
