0d1123abc199cc56a454f7d05cb802a177d9f211
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-05 21:50:08 +0900

    design: 로그인, 회원가입 버튼 디자인 수정

--- src/Components/Auth/LogOutSection.tsx
+++ src/Components/Auth/LogOutSection.tsx
@@ -3,11 +3,21 @@ import { Link } from 'react-router-dom';
 export default function LogOutSection() {
     return (
         <>
-            <div className={'flex gap-x-6 justify-self-end'}>
-                <Link className={'font-bold transition-all hover:text-violet-700'} to={'/sign_in'}>
+            <div className={'flex gap-x-4 justify-self-end'}>
+                <Link
+                    className={
+                        'rounded-lg bg-slate-100 px-4 py-2.5 text-[0.9rem] font-bold transition-all hover:bg-slate-200'
+                    }
+                    to={'/sign_in'}
+                >
                     로그인
                 </Link>
-                <Link className={'font-bold transition-all hover:text-violet-700'} to={'/sign_up'}>
+                <Link
+                    className={
+                        'rounded-lg bg-violet-600 px-4 py-2.5 text-[0.9rem] font-bold text-white transition-all hover:bg-violet-700'
+                    }
+                    to={'/sign_up'}
+                >
                     회원가입
                 </Link>
             </div>
