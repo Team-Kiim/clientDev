e3e10b112e7c03d6e37cf8493400e760cfcf5815
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-06 17:39:19 +0900

    feat: QnA 게시글 작성 페이지 들어간 후 작성될 게시글 아이디 받아오는 기능 추가

--- src/Pages/qnas/write/page.tsx
+++ src/Pages/qnas/write/page.tsx
@@ -1,11 +1,45 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import QnAWriteForm from '@/Pages/qnas/write/Components/QnAWriteForm.tsx';
 
 export default function Page() {
+    const postId = window.localStorage.getItem('currentQnAPostId');
+
+    const { data, isSuccess, isError, isLoading } = useQuery<number>({
+        queryKey: ['post', 'qna', 'initialId'],
+        queryFn: () => {
+            // return axios.post('/api/dev-post/init').then(response => response.data);
+            return Promise.resolve(1);
+        },
+        gcTime: 0,
+        enabled: !postId,
+    });
+
+    useEffect(() => {
+        return () => {
+            window.localStorage.removeItem('currentQnAPostId');
+        };
+    }, []);
+
+    if (isError) {
+        //TODO
+        // 알림
+        return null;
+    }
+
+    if (isSuccess) {
+        window.localStorage.setItem('currentQnAPostId', String(data));
+    }
+
+    if (isLoading) {
+        return null;
+    }
+
     return (
         <div className={'flex w-full min-w-[1500px] justify-center'}>
             <div className={'my-5 flex w-[45rem] flex-col gap-y-3'}>
-                <h1 className={'text-2xl font-extrabold'}>QnA 게시글 작성</h1>
-                <QnAWriteForm />
+                <QnAWriteForm postId={Number(postId ?? data)} />
             </div>
         </div>
     );
