d2962d9ef11079966d6022e7f8bd0c13c51d6a40
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-07 19:19:33 +0900

    design: 커뮤니티 게시글 목록 페이지 디자인 수정

--- src/Pages/community/page.tsx
+++ src/Pages/community/page.tsx
@@ -4,8 +4,8 @@ import PostAddButton from '@/Components/Post/PostAddButton.tsx';
 export default function Page() {
     return (
         <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
-            <div className={'w-[87.5rem]'}>
-                <div className={'flex items-center justify-between'}>
+            <div className={'w-[80rem]'}>
+                <div className={'flex items-center justify-end'}>
                     <PostAddButton />
                 </div>
                 <main className={'my-8'}>
