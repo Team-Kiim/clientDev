f5bae6d6c2a33680c549b758e250333b4fb9cc0b
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-10 22:23:40 +0900

    design: 로그아웃 버튼 디자인 수정

--- src/Components/Auth/LogInSection.tsx
+++ src/Components/Auth/LogInSection.tsx
@@ -28,7 +28,12 @@ export default function LogInSection() {
                         </div>
                     </div>
                 </Link>
-                <button className={'font-bold transition-all hover:text-violet-700'} type={'button'}>
+                <button
+                    className={
+                        'rounded-lg bg-slate-100 px-4 py-2.5 text-[0.9rem] font-bold transition-all hover:bg-slate-200'
+                    }
+                    type={'button'}
+                >
                     로그아웃
                 </button>
             </div>
