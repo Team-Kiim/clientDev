e7968d19559e54af37491c271de8be491bca34bb
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-13 02:05:45 +0900

    design: /qnas 페이지 width 수정

--- src/Pages/qnas/page.tsx
+++ src/Pages/qnas/page.tsx
@@ -6,7 +6,7 @@ import PostAddButton from '@/Components/Post/PostAddButton.tsx';
 export default function Page() {
     return (
         <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
-            <div className={'w-[87.5rem]'}>
+            <div className={'w-[80rem]'}>
                 <div className={'flex items-center justify-between'}>
                     <SearchFilter />
                     <SelectedCategoryList />
