82661883dba384f81d01dc0015a2a352f6d86e19
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-20 18:12:53 +0900

    fix: QnAPostInfo 객체타입에서 제외할 프로퍼티 명 수정
    -isMemberLiked에서 memberLiked
    -isMemberBookmarked에서 memberBookmarked

--- src/Pages/qnas/[boardId]/Components/PostDetails.tsx
+++ src/Pages/qnas/[boardId]/Components/PostDetails.tsx
@@ -4,7 +4,7 @@ import PostControl from '@/Pages/Components/PostView/PostControl.tsx';
 import PostSkillCategoryList from '@/Pages/qnas/[boardId]/Components/PostSkillCategoryList.tsx';
 import type { QnAPostInfo } from '@/Types/PostInfo.ts';
 
-type Props = Omit<QnAPostInfo, 'isMemberLiked' | 'likeCount' | 'isMemberBookmarked' | 'imageFileInfoDtoList'>;
+type Props = Omit<QnAPostInfo, 'memberLiked' | 'likeCount' | 'memberBookmarked' | 'imageFileInfoDtoList'>;
 
 export default function PostDetails({
     id,
