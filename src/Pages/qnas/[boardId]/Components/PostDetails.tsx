b985d38707dd1012cb1b91cbc3b271a8475e8f3c
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-20 18:07:35 +0900

    feat: PostSkillCategoryList 컴포넌트를 PostDetails 컴포넌트에 추가
    -기존 PostKeywordList 컴포넌트의 기능 대체

--- src/Pages/qnas/[boardId]/Components/PostDetails.tsx
+++ src/Pages/qnas/[boardId]/Components/PostDetails.tsx
@@ -1,7 +1,7 @@
 import PostMetaInfo from '@/Pages/Components/PostView/PostMetaInfo.tsx';
 import PostContent from '@/Pages/Components/PostView/PostContent.tsx';
 import PostControl from '@/Pages/Components/PostView/PostControl.tsx';
-import PostKeywordList from '@/Pages/qnas/[boardId]/Components/PostKeywordList.tsx';
+import PostSkillCategoryList from '@/Pages/qnas/[boardId]/Components/PostSkillCategoryList.tsx';
 import type { QnAPostInfo } from '@/Types/PostInfo.ts';
 
 type Props = Omit<QnAPostInfo, 'isMemberLiked' | 'likeCount' | 'isMemberBookmarked' | 'imageFileInfoDtoList'>;
@@ -14,7 +14,7 @@ export default function PostDetails({
     bodyContent,
     viewCount,
     createdTime,
-    skillCategories,
+    skillCategoryList,
     memberWritten,
 }: Props) {
     return (
@@ -29,7 +29,7 @@ export default function PostDetails({
             {memberWritten && <PostControl postId={id} />}
             <hr />
             <PostContent bodyContent={bodyContent} />
-            <PostKeywordList categories={skillCategories} />
+            <PostSkillCategoryList categories={skillCategoryList} />
         </article>
     );
 }
