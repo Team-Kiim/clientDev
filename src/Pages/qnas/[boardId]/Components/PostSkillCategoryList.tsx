c6eae6facdfc59c5dbd8bdcd1edaf2673962b535
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-20 18:06:47 +0900

    feat: PostSkillCategoryList 컴포넌트 추가

---
+++ src/Pages/qnas/[boardId]/Components/PostSkillCategoryList.tsx
@@ -0,0 +1,28 @@
+import { CATEGORIES } from '@/Constants/categories.ts';
+
+interface Props {
+    categories: {
+        parentSkillCategory: string;
+        childSkillCategory: string;
+    }[];
+}
+
+export default function PostSkillCategoryList({ categories }: Props) {
+    console.log(CATEGORIES.map(category => category.childCategories).flat());
+    const childSkillCategories = CATEGORIES.map(category => category.childCategories).flat();
+    return (
+        <ul className={'mb-5 mt-7 flex items-center gap-x-2.5'}>
+            {categories.map(category => {
+                return (
+                    <li className={'rounded-xl bg-gray-100 px-3 py-2 text-[0.8rem] font-bold text-gray-700'}>
+                        {
+                            childSkillCategories.find(
+                                childSkillCategory => childSkillCategory.value === category.childSkillCategory,
+                            ).label
+                        }
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
