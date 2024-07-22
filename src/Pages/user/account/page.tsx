3182903491843a8d3ce453c563c8269542a66039
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-22 22:32:10 +0900

    design: 계정 관리 페이지 디자인 수정

--- src/Pages/user/account/page.tsx
+++ src/Pages/user/account/page.tsx
@@ -3,11 +3,12 @@ import EmailUpdateForm from '@/Pages/user/account/Components/EmailUpdate/EmailUp
 export default function Page() {
     return (
         <>
-            <div className={'mb-6 flex flex-col gap-y-7'}>
+            <div className={'mb-6 flex w-4/6 flex-col gap-y-6'}>
                 <h1 className={'flex text-xl font-extrabold'}>계정 관리</h1>
-                <div className={'flex flex-col gap-y-5'}>
+                <div className={'flex flex-col gap-y-4'}>
                     <h3 className={'text-[0.95rem] font-bold'}>이메일 변경</h3>
                     <EmailUpdateForm />
+                    <hr />
                 </div>
             </div>
         </>
