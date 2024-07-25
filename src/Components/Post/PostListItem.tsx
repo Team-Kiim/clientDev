611f793976305e2537ad1d4d03f6077d91ae95ff
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-25 14:23:51 +0900

    design: 게시글 목록 아이템 디자인 수정

--- src/Components/Post/PostListItem.tsx
+++ src/Components/Post/PostListItem.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
-import { CalendarIcon } from '@heroicons/react/24/outline';
-import { BiSolidLike } from 'react-icons/bi';
-import { TiEye } from 'react-icons/ti';
+import { HiEye, HiHeart, HiOutlineCalendar } from 'react-icons/hi2';
 import formatNumber from '@/Utils/formatNumber.ts';
 import type { Post } from '@/Types/Post.ts';
 
@@ -28,11 +26,7 @@ export default function PostListItem({ post, postType }: Props) {
     } = post;
 
     return (
-        <li
-            className={
-                'h-[26rem] rounded-md border border-gray-100 shadow-lg transition-all hover:-translate-y-2 hover:shadow-xl'
-            }
-        >
+        <li className={'h-[25rem] rounded-md border border-slate-300 transition-all hover:-translate-y-2'}>
             <Link to={`${postType === 'qnas' ? '/qnas' : '/community'}/${id}`}>
                 <div className={'flex h-full flex-col'}>
                     {imagePath !== null && (
@@ -44,21 +38,21 @@ export default function PostListItem({ post, postType }: Props) {
                             />
                         </div>
                     )}
-                    <div className={'mb-1 mt-2.5 flex gap-x-1.5 px-3.5 text-gray-600'}>
-                        <CalendarIcon className={'size-4'} />
+                    <div className={'mb-1 mt-2.5 flex gap-x-1.5 px-3.5 text-slate-600'}>
+                        <HiOutlineCalendar className={'size-4'} />
                         <span
                             className={'text-[0.75rem]'}
                         >{`${createdTime[0]}년 ${createdTime[1]}월 ${createdTime[2]}일`}</span>
                     </div>
                     <div className={'flex flex-1 flex-col gap-y-1.5 px-3.5'}>
                         <h1 className={'line-clamp-1 text-lg font-bold'}>{title}</h1>
-                        <p className={'line-clamp-4 text-[0.85rem] text-gray-500'}>
+                        <p className={'line-clamp-3 text-[0.85rem] text-slate-500'}>
                             {bodyContent.replace(/<[^>]+>/g, '')}
                         </p>
                     </div>
                     <div
                         className={
-                            'flex items-center justify-between rounded-b-md border-t border-gray-200 bg-gray-50 px-3.5 py-2'
+                            'flex items-center justify-between rounded-b-md border-t border-slate-200 bg-slate-50 px-3.5 py-2'
                         }
                     >
                         <div className={'flex items-center gap-x-2'}>
@@ -71,13 +65,13 @@ export default function PostListItem({ post, postType }: Props) {
                             </div>
                             <span className={'line-clamp-1 text-[0.82rem] font-bold'}>{nickname}</span>
                         </div>
-                        <div className={'flex items-center gap-x-4'}>
+                        <div className={'flex items-center gap-x-3'}>
                             <div className={'flex items-center gap-x-1'}>
-                                <BiSolidLike className={'size-4 text-gray-700'} />
+                                <HiHeart className={'size-4 text-slate-600'} />
                                 <span className={'text-[0.8rem] font-bold'}>{formatNumber(likeCount, 0)}</span>
                             </div>
                             <div className={'flex items-center gap-x-1'}>
-                                <TiEye className={'size-5 text-gray-700'} />
+                                <HiEye className={'size-4 text-slate-600'} />
                                 <span className={'text-[0.8rem] font-bold'}>{formatNumber(viewCount, 0)}</span>
                             </div>
                         </div>
