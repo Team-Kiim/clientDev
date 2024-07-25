e63555c25d370ecc7bbaeccba7814a8c84211b90
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-25 12:46:46 +0900

    design: 회원가입 페이지 width 수정

--- src/Pages/sign_up/page.tsx
+++ src/Pages/sign_up/page.tsx
@@ -4,7 +4,7 @@ import OAuthSection from '@/Components/Auth/OAuthSection/OAuthSection.tsx';
 
 export default function Page() {
     return (
-        <div className={'mx-auto my-16 flex w-[27rem] flex-col items-center p-5'}>
+        <div className={'mx-auto my-16 flex w-[25rem] flex-col items-center p-5'}>
             <div className={'mb-1.5 flex w-full flex-col items-center gap-y-5'}>
                 <div className={'flex w-full flex-col items-center gap-y-10'}>
                     <Link className={'logo text-4xl text-violet-700'} to={'/'}>
