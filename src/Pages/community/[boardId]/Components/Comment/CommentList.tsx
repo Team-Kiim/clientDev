c906f03cdd12ecdb4bafec227b50773dfc29abff
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-27 15:06:38 +0900

    feat: 댓글 삭제 부분 추가

--- src/Pages/community/[boardId]/Components/Comment/CommentList.tsx
+++ src/Pages/community/[boardId]/Components/Comment/CommentList.tsx
@@ -1,15 +1,77 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 import CommentListItem from '@/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx';
-import type { CommentInfo } from '@/Types/PostInfo.ts';
+import TOAST_OPTIONS from '@/Constants/toastOptions.ts';
+import type { CommentInfo, PostInfo } from '@/Types/PostInfo.ts';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
+    postId: string;
     commentInfoDtoList: CommentInfo[];
 }
 
-export default function CommentList({ commentInfoDtoList }: Props) {
+export default function CommentList({ postId, commentInfoDtoList }: Props) {
+    const queryClient = useQueryClient();
+
+    const [previousCommentList, setPreviousCommentList] = useState(commentInfoDtoList);
+
+    const { mutate: mutateDeleteComment } = useMutation({
+        mutationFn(commentId: number) {
+            return axios.delete(`/api/comment/delete/${commentId}`, { timeout: 5000 });
+        },
+
+        onSuccess() {
+            const updatedCommentList = queryClient.getQueryData<PostInfo>(['post', postId]).commentInfoDtoList;
+            setPreviousCommentList(updatedCommentList);
+        },
+
+        onError() {
+            toast.error(
+                <div className={'text-[0.85rem]'}>
+                    댓글을 삭제할 수 없습니다. <br /> 잠시 후 다시 시도해 주세요.
+                </div>,
+                TOAST_OPTIONS,
+            );
+            queryClient.setQueryData<PostInfo>(['post', postId], postData => {
+                return {
+                    ...postData,
+                    commentInfoDtoList: previousCommentList,
+                };
+            });
+        },
+
+        // onSettled() {
+        //     return queryClient.invalidateQueries({ queryKey: ['post', postId] });
+        // },
+    });
+
+    const handleCommentDeleteButtonClick = async (commentId: number) => {
+        if (window.confirm('정말로 댓글을 삭제하시겠습니까?')) {
+            await queryClient.cancelQueries({ queryKey: ['post', postId] });
+
+            queryClient.setQueryData<PostInfo>(['post', postId], postData => {
+                return {
+                    ...postData,
+                    commentInfoDtoList: postData.commentInfoDtoList.filter(comment => comment.id !== commentId),
+                };
+            });
+
+            mutateDeleteComment(commentId);
+        }
+    };
+
     return (
         <ul className={'flex flex-col gap-y-2.5'}>
             {commentInfoDtoList.map(commentInfo => {
-                return <CommentListItem key={commentInfo.id} commentInfo={commentInfo} />;
+                return (
+                    <CommentListItem
+                        key={commentInfo.id}
+                        commentInfo={commentInfo}
+                        onCommentDeleteButtonClick={handleCommentDeleteButtonClick}
+                    />
+                );
             })}
         </ul>
     );
--- src/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx
+++ src/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx
@@ -3,9 +3,10 @@ import type { CommentInfo } from '@/Types/PostInfo.ts';
 
 interface Props {
     commentInfo: CommentInfo;
+    onCommentDeleteButtonClick(commentId: number): void;
 }
 
-export default function CommentListItem({ commentInfo }: Props) {
+export default function CommentListItem({ commentInfo, onCommentDeleteButtonClick }: Props) {
     const { VITE_SERVER_URL } = import.meta.env;
     const { profileImagePath, profileImageName } = commentInfo;
 
@@ -19,12 +20,26 @@ export default function CommentListItem({ commentInfo }: Props) {
                         alt={profileImageName}
                     />
                 </div>
-                <div className={'flex flex-col gap-y-3'}>
-                    <div className={'flex flex-col gap-y-0.5'}>
-                        <span className={'text-[0.9rem] font-bold'}>{commentInfo.nickname}</span>
-                        <span className={'text-[0.7rem] font-bold text-slate-400'}>
-                            {commentInfo.createdTime[0]}년 {commentInfo.createdTime[1]}월 {commentInfo.createdTime[2]}일
-                        </span>
+                <div className={'flex flex-1 flex-col gap-y-3'}>
+                    <div className={'flex items-center'}>
+                        <div className={'flex flex-1 flex-col gap-y-0.5'}>
+                            <span className={'text-[0.9rem] font-bold'}>{commentInfo.nickname}</span>
+                            <span className={'text-[0.7rem] font-bold text-slate-400'}>
+                                {commentInfo.createdTime[0]}년 {commentInfo.createdTime[1]}월{' '}
+                                {commentInfo.createdTime[2]}일
+                            </span>
+                        </div>
+                        <div className={'gap-x-2'}>
+                            <button
+                                className={'text-[0.8rem] font-bold text-slate-500'}
+                                type={'button'}
+                                onClick={() => {
+                                    onCommentDeleteButtonClick(commentInfo.id);
+                                }}
+                            >
+                                삭제
+                            </button>
+                        </div>
                     </div>
                     <div
                         className={'prose prose-sm max-w-full text-[0.87rem] text-black'}
