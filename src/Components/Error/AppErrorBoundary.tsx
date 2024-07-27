cbd2a8ebb06848ca0cff734c312a2bf821ac220d
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-27 10:59:46 +0900

    feat: 클라이언트, 서버 에러 처리하기 위한 컴포넌트 추가

---
+++ src/Components/Error/AppErrorBoundary.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import AppErrorFallback from '@/Components/Error/AppErrorFallback.tsx';
+
+interface Props {
+    children: ReactNode;
+}
+
+export default function AppErrorBoundary({ children }: Props) {
+    const { pathname } = useLocation();
+    const { reset } = useQueryErrorResetBoundary();
+    reset();
+
+    return (
+        <ErrorBoundary
+            key={pathname}
+            onReset={() => {
+                reset();
+            }}
+            FallbackComponent={AppErrorFallback}
+        >
+            {children}
+        </ErrorBoundary>
+    );
+}
