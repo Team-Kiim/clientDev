bec45a790b6fc5193d5dab77c0a72fca576b0bd5
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-27 14:58:51 +0900

    feat: react-toastify 패키지 사용을 위한 ToastContainer 컴포넌트 포함

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Modal from 'react-modal';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContainer } from 'react-toastify';
 import router from '@/router.tsx';
 
 Modal.setAppElement('#root');
@@ -20,6 +21,7 @@ function App() {
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
             <ReactQueryDevtools />
+            <ToastContainer />
         </QueryClientProvider>
     );
 }
