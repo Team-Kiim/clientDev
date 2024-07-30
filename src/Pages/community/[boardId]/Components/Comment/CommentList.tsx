749b3908be813e5f97bdc03061ebd71fef1345f7
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-30 13:51:30 +0900

    refactor: 댓글 삭제 로직을 커스텀 훅으로 분리

--- src/Pages/community/[boardId]/Components/Comment/CommentList.tsx
+++ src/Pages/community/[boardId]/Components/Comment/CommentList.tsx
@@ -1,10 +1,7 @@
-import axios from 'axios';
 import { useState } from 'react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { toast } from 'react-toastify';
 import CommentListItem from '@/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx';
-import TOAST_OPTIONS from '@/Constants/toastOptions.ts';
-import type { CommentInfo, PostInfo } from '@/Types/PostInfo.ts';
+import { useDeleteComment } from '@/Pages/community/[boardId]/Components/Comment/Hooks/useDeleteComment.tsx';
+import type { CommentInfo } from '@/Types/PostInfo.ts';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
@@ -13,12 +10,8 @@ interface Props {
 }
 
 export default function CommentList({ postId, commentInfoDtoList }: Props) {
-    const queryClient = useQueryClient();
-
     const [isCommentEditFormOpen, setIsCommentEditFormOpen] = useState(false);
     const [idOfCommentToEdit, setIdOfCommentToEdit] = useState(0);
-
-    const [previousCommentList, setPreviousCommentList] = useState(commentInfoDtoList);
 
     const handleCommentEditButtonClick = (commentId: number) => {
         if (isCommentEditFormOpen) {
@@ -34,48 +27,11 @@ export default function CommentList({ postId, commentInfoDtoList }: Props) {
         setIsCommentEditFormOpen(true);
     };
 
-    const { mutate: mutateDeleteComment } = useMutation({
-        mutationFn(commentId: number) {
-            return axios.delete(`/api/comment/delete/${commentId}`, { timeout: 5000 });
-        },
-
-        onSuccess() {
-            const updatedCommentList = queryClient.getQueryData<PostInfo>(['post', postId]).commentInfoDtoList;
-            setPreviousCommentList(updatedCommentList);
-        },
-
-        onError() {
-            toast.error(
-                <div className={'text-[0.85rem]'}>
-                    댓글을 삭제할 수 없습니다. <br /> 잠시 후 다시 시도해 주세요.
-                </div>,
-                TOAST_OPTIONS,
-            );
-            queryClient.setQueryData<PostInfo>(['post', postId], postData => {
-                return {
-                    ...postData,
-                    commentInfoDtoList: previousCommentList,
-                };
-            });
-        },
-
-        // onSettled() {
-        //     return queryClient.invalidateQueries({ queryKey: ['post', postId] });
-        // },
-    });
+    const { mutate } = useDeleteComment(postId);
 
     const handleCommentDeleteButtonClick = async (commentId: number) => {
         if (window.confirm('정말로 댓글을 삭제하시겠습니까?')) {
-            await queryClient.cancelQueries({ queryKey: ['post', postId] });
-
-            queryClient.setQueryData<PostInfo>(['post', postId], postData => {
-                return {
-                    ...postData,
-                    commentInfoDtoList: postData.commentInfoDtoList.filter(comment => comment.id !== commentId),
-                };
-            });
-
-            mutateDeleteComment(commentId);
+            mutate(commentId);
         }
     };
 
