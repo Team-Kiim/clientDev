d2520b0000922b0daa3d890c60f5cd450577d086
Kim Kangmin <rlarkdals1202@gmail.com>
2024-07-30 13:47:07 +0900

    feat: 댓글 삭제 관련 커스텀 훅 추가

---
+++ src/Pages/community/[boardId]/Components/Comment/Hooks/useDeleteComment.tsx
@@ -0,0 +1,56 @@
+import axios, { AxiosError } from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import TOAST_OPTIONS from '@/Constants/toastOptions.ts';
+import type { PostInfo } from '@/Types/PostInfo.ts';
+
+export const useDeleteComment = (postId: string) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (commentId: number) => {
+            return axios.delete(`/api/comment/delete/${commentId}`, { timeout: 5000 });
+        },
+
+        onMutate: async (commendId: number) => {
+            await queryClient.cancelQueries({
+                queryKey: ['post', postId],
+            });
+
+            const previousCommentList = queryClient.getQueryData<PostInfo>(['post', postId]).commentInfoDtoList;
+
+            queryClient.setQueryData<PostInfo>(['post', postId], oldData => {
+                return {
+                    ...oldData,
+                    commentInfoDtoList: previousCommentList.filter(commentInfo => {
+                        return commentInfo.id !== commendId;
+                    }),
+                };
+            });
+
+            return { previousCommentList };
+        },
+
+        onError: (error: AxiosError, _, context) => {
+            console.error(error);
+            toast.error(
+                <div className={'text-[0.85rem]'}>
+                    댓글을 삭제할 수 없습니다. <br /> 잠시 후 다시 시도해주세요.
+                </div>,
+                TOAST_OPTIONS,
+            );
+            queryClient.setQueryData<PostInfo>(['post', postId], oldData => {
+                return {
+                    ...oldData,
+                    commentInfoDtoList: context.previousCommentList,
+                };
+            });
+        },
+
+        onSettled: () => {
+            return queryClient.invalidateQueries({
+                queryKey: ['post', postId],
+            });
+        },
+    });
+};
