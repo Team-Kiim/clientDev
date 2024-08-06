b317731c14df4cae64f8293c8b77ca043a976249
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-06 15:50:44 +0900

    refactor: 모달 바깥 영역 클릭 이벤트 등록 조건 추가

--- src/Hooks/useModal.ts
+++ src/Hooks/useModal.ts
@@ -10,16 +10,18 @@ export const useModal = <T extends HTMLElement>({ canCloseOnOutsideClick }: Moda
     const modalRef = useRef<T>(null);
 
     useEffect(() => {
-        const handleOutSideClick = (event: MouseEvent) => {
-            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-                setIsModalOpen(false);
-            }
-        };
+        if (canCloseOnOutsideClick) {
+            const handleOutSideClick = (event: MouseEvent) => {
+                if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+                    setIsModalOpen(false);
+                }
+            };
 
-        document.addEventListener('mousedown', handleOutSideClick);
-        return () => {
-            document.removeEventListener('mousedown', handleOutSideClick);
-        };
+            document.addEventListener('mousedown', handleOutSideClick);
+            return () => {
+                document.removeEventListener('mousedown', handleOutSideClick);
+            };
+        }
     }, [modalRef]);
 
     return {
