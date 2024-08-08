9f80c4ffb7c5b318963d199e4c1288cc8f918391
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-08 15:40:57 +0900

    remove: 사용되지 않는 파일들 삭제

--- src/Components/Auth/OAuthSection/GoogleSection.tsx
+++
@@ -1,9 +0,0 @@
-import { FcGoogle } from 'react-icons/fc';
-
-export default function GoogleSection() {
-    return (
-        <button className={'flex size-11 items-center justify-center rounded-md border border-gray-300 bg-white p-0.5'}>
-            <FcGoogle className={'size-6'} />
-        </button>
-    );
-}
