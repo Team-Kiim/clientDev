e99972975e828a658d4ab64cf041bb75855bedcf
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-08 12:19:42 +0900

    feat: 채팅 목록 아이템 컴포넌트 추가

---
+++ src/Components/MyChat/MyChatModal/ChatRoom/ChatBubbleListItem.tsx
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+
+interface Props {
+    memberSent?: boolean;
+    messageType: string;
+}
+
+export default function ChatBubbleListItem({ memberSent, messageType }: Props) {
+    if (messageType === 'EXIT' || messageType === 'ENTER') {
+        return (
+            <li
+                className={
+                    'mx-auto w-fit list-none rounded-3xl border border-slate-300 bg-white px-4 py-0.5 text-center text-[0.75rem] text-black'
+                }
+            >
+                aaaaa1234님이 {messageType === 'EXIT' ? '나갔습니다.' : '들어왔습니다.'}
+            </li>
+        );
+    }
+
+    return (
+        <li className={`chat ${memberSent ? 'chat-start' : 'chat-end'}`}>
+            <div className={'avatar chat-image'}>
+                <div className={'size-9 rounded-full'}>
+                    <img src={faker.image.avatarGitHub()} alt={'testImage'} />
+                </div>
+            </div>
+            <div className={'chat-header mb-1 flex items-center text-[0.75rem]'}>
+                <span className={'mx-2 font-bold'}>kkangasdf12</span>
+            </div>
+            <div
+                className={`chat-bubble flex max-w-[200px] flex-col justify-center ${!memberSent ? 'bg-slate-200' : 'text-white'} text-[0.8rem] text-black`}
+            >
+                안녕하세요. 테스트입니다. 안녕하세요. 테스트입니다.
+            </div>
+            <div className={'chat-footer'}>
+                <time className={'text-[0.65rem] opacity-80'}>
+                    {2024}년 {12}월 {2}일 · 오후 {12}:{45}
+                </time>
+            </div>
+        </li>
+    );
+}
