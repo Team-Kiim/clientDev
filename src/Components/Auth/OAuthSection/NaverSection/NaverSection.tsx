6923eff5abc764fe6a2f7e922c33c9c5ed4aeaad
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-08 15:55:49 +0900

    design: 네이버 OAuth 버튼 디자인 수정

--- src/Components/Auth/OAuthSection/NaverSection/NaverSection.tsx
+++ src/Components/Auth/OAuthSection/NaverSection/NaverSection.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react';
-import { SiNaver } from 'react-icons/si';
 
 const initNaverLogin = () => {
     const { VITE_DEV_URL, VITE_NAVER_CLIENT_ID } = import.meta.env;
@@ -29,14 +28,35 @@ export default function NaverSection() {
     return (
         <>
             <button
-                className={'flex size-11 items-center justify-center rounded-md bg-[#03C75A] p-0.5 text-white'}
+                className={'flex w-full items-center justify-between rounded-xl bg-[#03C75A] px-5 py-[13px] text-white'}
                 onClick={() => {
                     // 아이디가 naverIdLogin 인 div 요소 안에 a 요소가 존재하므로, div#naverIdLogin 요소에 클릭 이벤트 핸들러를 등록하면 안되고,
                     // 자식 요소, 즉 a 요소에 클릭 이벤트 핸들러를 등록해야 한다.
                     (naverIdLoginRef.current.firstElementChild as HTMLAnchorElement).click();
                 }}
             >
-                <SiNaver />
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='800px'
+                    height='800px'
+                    viewBox='0 0 512 512'
+                    version='1.1'
+                    fill='#ffffff'
+                    stroke='#ffffff'
+                    className={'size-6'}
+                >
+                    <g id='SVGRepo_bgCarrier' stroke-width='0' />
+
+                    <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round' />
+
+                    <g id='SVGRepo_iconCarrier'>
+                        <path
+                            fill='#ffffff'
+                            d='M9 32V480H181.366V255.862L331.358 480H504V32H331.358V255.862L181.366 32H9Z'
+                        />
+                    </g>
+                </svg>
+                <span className={'flex-1 text-center text-sm font-bold'}>네이버로 시작하기</span>{' '}
             </button>
             <div className={'hidden'} id={'naverIdLogin'} ref={naverIdLoginRef} />
         </>
