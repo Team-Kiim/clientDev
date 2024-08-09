eaf6ccf54badf87065178bdbd94c0fe94c3b41fe
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-09 15:53:38 +0900

    feat: 선택된 카테고리 개별 삭제 기능 구현

--- src/Components/SelectedCategoryList.tsx
+++ src/Components/SelectedCategoryList.tsx
@@ -1,10 +1,21 @@
 import { useSearchParams } from 'react-router-dom';
+import { HiXMark } from 'react-icons/hi2';
 import { CATEGORIES } from '@/Constants/categories.ts';
 
 export default function SelectedCategoryList() {
-    const [searchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
     const childCategories = CATEGORIES.map(category => category.childCategories).flat();
     const selectedChildCategories = searchParams.getAll('category');
+
+    const deleteCategory = (category: string) => {
+        searchParams.delete('category');
+        for (const selectedChildCategory of selectedChildCategories) {
+            if (category !== selectedChildCategory) {
+                searchParams.append('category', selectedChildCategory);
+            }
+        }
+        setSearchParams(searchParams);
+    };
 
     return (
         <div className={'mx-6 shrink-0 flex-grow basis-0 overflow-x-auto'}>
@@ -12,9 +23,23 @@ export default function SelectedCategoryList() {
                 {selectedChildCategories.map(selectedChildCategory => {
                     return (
                         <li
-                            className={'shrink-0 rounded-xl border border-slate-300 px-4 py-2 text-[0.85rem] font-bold'}
+                            className={
+                                'flex shrink-0 items-center justify-between gap-x-2 rounded-xl border border-slate-300 px-4 py-2 text-[0.85rem] font-bold'
+                            }
                         >
-                            {childCategories.find(childCategory => childCategory.value === selectedChildCategory).label}
+                            <span>
+                                {
+                                    childCategories.find(childCategory => childCategory.value === selectedChildCategory)
+                                        .label
+                                }
+                            </span>
+                            <button
+                                onClick={() => {
+                                    deleteCategory(selectedChildCategory);
+                                }}
+                            >
+                                <HiXMark className={'size-4 text-slate-500'} />
+                            </button>
                         </li>
                     );
                 })}
