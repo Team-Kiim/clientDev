2cb4b820a33f0b996a8714c99d03fdc4787b9e12
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-12 14:49:18 +0900

    design: 댓글 등록 버튼 디자인 수정

--- src/Pages/community/[boardId]/Components/Comment/CommentWriteForm.tsx
+++ src/Pages/community/[boardId]/Components/Comment/CommentWriteForm.tsx
@@ -3,7 +3,6 @@ import dompurify from 'dompurify';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { SubmitHandler, useController, useForm } from 'react-hook-form';
 import { AiOutlineExclamationCircle } from 'react-icons/ai';
-import { RiArrowUpLine } from 'react-icons/ri';
 import CommentEditor from '@/Pages/community/[boardId]/Components/Comment/CommentEditor/CommentEditor.tsx';
 
 interface Props {
@@ -63,22 +62,24 @@ export default function CommentWriteForm({ postId }: Props) {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className={'flex h-56 flex-col rounded-xl border border-slate-200'}>
+        <form onSubmit={handleSubmit(onSubmit)} className={'flex flex-col gap-y-4'}>
+            <div className={'flex h-60 flex-col rounded-xl border border-slate-200'}>
                 <CommentEditor submitCount={submitCount} onChange={onChange} onBlur={onBlur} />
-                <div className={'flex items-center p-2.5'}>
-                    {errors?.commentValue?.message && errors?.commentValue.type === 'required' && (
-                        <div className={'m-0.5 mr-auto flex items-center gap-x-1 justify-self-start text-rose-700'}>
-                            <AiOutlineExclamationCircle className={'size-4'} />
-                            <p className={'text-[0.8rem] font-bold'}>{errors.commentValue.message}</p>
-                        </div>
-                    )}
-                    <div className={'tooltip tooltip-bottom ml-auto'} data-tip={'댓글 작성'}>
-                        <button className={'rounded-full p-1.5 transition-all hover:bg-slate-100'} type={'submit'}>
-                            <RiArrowUpLine className={'size-7'} />
-                        </button>
+            </div>
+            <div className={'flex items-center'}>
+                {errors?.commentValue?.message && errors?.commentValue.type === 'required' && (
+                    <div className={'m-0.5 mr-auto flex items-center gap-x-1 justify-self-start text-red-500'}>
+                        <AiOutlineExclamationCircle className={'size-4'} />
+                        <p className={'text-[0.8rem]'}>{errors.commentValue.message}</p>
                     </div>
-                </div>
+                )}
+                <button
+                    className={
+                        'ml-auto rounded-lg border border-violet-300 bg-violet-50 px-3 py-1.5 text-[0.85rem] font-bold text-violet-700'
+                    }
+                >
+                    등록
+                </button>
             </div>
         </form>
     );
