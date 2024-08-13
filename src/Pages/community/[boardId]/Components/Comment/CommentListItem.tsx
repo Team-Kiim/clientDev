d2072f27a2afd336e73abe8097d0be90ec015250
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-13 18:49:52 +0900

    feat: 댓글 내용 내 소스 코드 하이라이트 기능 추가

--- src/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx
+++ src/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx
@@ -1,4 +1,42 @@
 import dompurify from 'dompurify';
+import Prism from 'prismjs';
+import { useEffect } from 'react';
+import 'prism-themes/themes/prism-one-dark.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/components/prism-arduino';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-csp';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-diff';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-graphql';
+import 'prismjs/components/prism-ini';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-kotlin';
+import 'prismjs/components/prism-less';
+import 'prismjs/components/prism-lua';
+import 'prismjs/components/prism-makefile';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-objectivec';
+import 'prismjs/components/prism-markup-templating.js';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-perl';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-r';
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-rust';
+import 'prismjs/components/prism-shell-session';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-swift';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-wasm';
+import 'prismjs/components/prism-xml-doc';
+import 'prismjs/components/prism-yaml';
 import CommentEditForm from '@/Pages/community/[boardId]/Components/Comment/CommentEditForm.tsx';
 import type { CommentInfo } from '@/Types/PostInfo.ts';
 
@@ -24,7 +62,13 @@ export default function CommentListItem({
     const { VITE_SERVER_URL } = import.meta.env;
     const { profileImagePath, profileImageName } = commentInfo;
 
-    console.log(5);
+    useEffect(() => {
+        Array.from(document.getElementsByTagName('pre')).forEach($preElement => {
+            $preElement.className += 'line-numbers';
+        });
+        Prism.highlightAll();
+    }, [isCommentEditFormOpen]);
+
     return (
         <li className={'flex flex-col border-b border-slate-300 py-3.5 last:border-none'}>
             <div className={'flex gap-x-4'}>
@@ -35,7 +79,7 @@ export default function CommentListItem({
                         alt={profileImageName}
                     />
                 </div>
-                <div className={'flex flex-1 flex-col gap-y-3'}>
+                <div className={'flex flex-1 flex-col gap-y-4'}>
                     <div className={'flex items-center'}>
                         <div className={'flex flex-1 flex-col gap-y-0.5'}>
                             <span className={'text-[0.9rem] font-bold'}>{commentInfo.nickname}</span>
@@ -75,7 +119,7 @@ export default function CommentListItem({
                         />
                     ) : (
                         <div
-                            className={'prose prose-sm max-w-full text-[0.87rem] text-black'}
+                            className={'prose prose-sm max-w-full text-[0.87rem] text-black [&_code]:text-[0.87rem]'}
                             dangerouslySetInnerHTML={{ __html: dompurify.sanitize(commentInfo.content) }}
                         />
                     )}
