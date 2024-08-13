772b75431c41500f108183ffed3e97ec7ac02d86
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-13 18:23:44 +0900

    feat: 게시글 삭제 기능 구현

--- src/Pages/Components/PostView/PostControl.tsx
+++ src/Pages/Components/PostView/PostControl.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
+import Swal from 'sweetalert2';
 
 interface Props {
     postId: number;
@@ -10,21 +12,52 @@ export default function PostControl({ postId }: Props) {
 
     const { mutate: deleteMutate } = useMutation({
         mutationFn: () => {
-            //TODO
-            // 게시글 삭제 API 요청
-            return Promise.resolve(1);
+            return axios.delete(`/api/post/delete/${postId}`);
         },
 
-        onSuccess: () => {
-            window.alert('성공적으로 삭제하였습니다.');
+        onSuccess: async () => {
+            await Swal.fire({
+                icon: 'success',
+                text: '성공적으로 삭제하였습니다.',
+                confirmButtonText: '확인',
+                customClass: {
+                    confirmButton: 'text-white font-bold bg-violet-600',
+                },
+            });
             navigate('/');
         },
 
-        onError: () => {
-            window.alert('게시글을 삭제할 수 없습니다. 잠시 후 다시 시도해주세요.');
+        onError: async () => {
+            await Swal.fire({
+                icon: 'error',
+                text: '게시글을 삭제할 수 없습니다. 잠시 후 다시 시도해주세요.',
+                customClass: {
+                    confirmButton: 'text-white font-bold bg-violet-600',
+                },
+            });
             return;
         },
     });
+
+    const handleDeletePostButtonClick = () => {
+        Swal.fire({
+            icon: 'warning',
+            text: '정말로 게시글을 삭제하시겠습니까?',
+            showCancelButton: true,
+            confirmButtonText: '삭제',
+            cancelButtonText: '취소',
+            confirmButtonColor: '#7c3aed',
+            cancelButtonColor: '#f1f5f9',
+            customClass: {
+                cancelButton: 'text-black font-bold',
+                confirmButton: 'font-bold',
+            },
+        }).then(result => {
+            if (result.isConfirmed) {
+                deleteMutate();
+            }
+        });
+    };
 
     return (
         <div className={'flex w-full justify-end gap-x-2 text-[0.9rem]'}>
@@ -38,15 +71,7 @@ export default function PostControl({ postId }: Props) {
                 <span className={'hover:underline hover:underline-offset-4'}>수정</span>
             </button>
             <span>•</span>
-            <button
-                className={'flex items-center gap-x-1'}
-                type={'button'}
-                onClick={() => {
-                    if (window.confirm('정말로 삭제하시겠습니까?')) {
-                        deleteMutate();
-                    }
-                }}
-            >
+            <button className={'flex items-center gap-x-1'} type={'button'} onClick={handleDeletePostButtonClick}>
                 <span className={'hover:underline hover:underline-offset-4'}>삭제</span>
             </button>
         </div>
