db9bfd81eaf88ef1e421429227c26e563c54ca41
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-14 15:04:34 +0900

    feat: 팔로잉, 팔로워 목록 요청 관련 에러 바운더리, 로딩 fallback 추가

--- src/Pages/user/social/page.tsx
+++ src/Pages/user/social/page.tsx
@@ -1,36 +1,32 @@
-import { useOutletContext } from 'react-router-dom';
+import { Suspense } from 'react';
+import SocialApiErrorBoundary from '@/Pages/user/social/Components/SocialApiErrorBoundary.tsx';
 import FollowingList from '@/Pages/user/social/Components/Following/FollowingList.tsx';
 import FollowerList from '@/Pages/user/social/Components/Follower/FollowerList.tsx';
-import { User } from '@/Types/User.ts';
-
-interface UserDataContext {
-    userData: User;
-}
 
 export default function Page() {
-    const { userData } = useOutletContext<UserDataContext>();
-
     return (
         <>
             <div className={'mb-6 flex flex-col gap-y-7'}>
                 <h1 className={'flex text-xl font-extrabold'}>소셜 관리</h1>
                 <div className={'flex flex-col gap-y-1'}>
-                    <div className={'flex items-center gap-x-2'}>
-                        <h3 className={'text-[0.95rem] font-bold'}>팔로잉 목록</h3>
-                        <span className={'text-[0.8rem] font-extrabold text-slate-500'}>(팔로잉 수 : {10})</span>
-                    </div>
-                    <div className={'rounded-lg border border-slate-200'}>
-                        <FollowingList />
+                    <h3 className={'text-[0.95rem] font-bold'}>팔로잉 목록</h3>
+                    <div className={'relative h-96 rounded-lg border border-slate-200'}>
+                        <SocialApiErrorBoundary relationshipType={'following'}>
+                            <Suspense fallback={<>로딩</>}>
+                                <FollowingList />
+                            </Suspense>
+                        </SocialApiErrorBoundary>
                     </div>
                 </div>
                 <hr />
                 <div className={'flex flex-col gap-y-1'}>
-                    <div className={'flex items-center gap-x-2'}>
-                        <h3 className={'text-[0.95rem] font-bold'}>팔로워 목록</h3>
-                        <span className={'text-[0.8rem] font-extrabold text-slate-500'}>(팔로우 수 : {10})</span>
-                    </div>
-                    <div className={'rounded-lg border border-slate-200'}>
-                        <FollowerList />
+                    <h3 className={'text-[0.95rem] font-bold'}>팔로워 목록</h3>
+                    <div className={'relative h-96 rounded-lg border border-slate-200'}>
+                        <SocialApiErrorBoundary relationshipType={'follower'}>
+                            <Suspense fallback={<>로딩</>}>
+                                <FollowerList />
+                            </Suspense>
+                        </SocialApiErrorBoundary>
                     </div>
                 </div>
             </div>
