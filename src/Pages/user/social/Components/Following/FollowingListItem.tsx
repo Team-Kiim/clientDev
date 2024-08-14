44a678edd3696ce1907b488d85bfc00975fb314d
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-14 15:02:03 +0900

    feat: 팔로잉 목록 가져오는 기능 구현

--- src/Pages/user/social/Components/Following/FollowingListItem.tsx
+++ src/Pages/user/social/Components/Following/FollowingListItem.tsx
@@ -1,24 +1,39 @@
 import { Link } from 'react-router-dom';
-import { faker } from '@faker-js/faker';
+import type { SocialMediaUser } from '@/Types/SocialMediaUser.ts';
 
 interface Props {
+    socialMediaUser: SocialMediaUser;
     onUnfollowButtonClick(): void;
+    onFollowingListItemClick(memberId: number): void;
 }
 
-export default function FollowingListItem({ onUnfollowButtonClick }: Props) {
+export default function FollowingListItem({ socialMediaUser, onUnfollowButtonClick, onFollowingListItemClick }: Props) {
+    const { email, nickname, profileImagePath, profileImageName, memberId } = socialMediaUser;
+
+    const { VITE_SERVER_URL } = import.meta.env;
+
     return (
-        <li className={'flex gap-x-4 border-b border-slate-200 p-3.5 last:border-none'}>
+        <li
+            className={'flex gap-x-4 border-b border-slate-200 p-3.5 last:border-none'}
+            onClick={() => {
+                onFollowingListItemClick(memberId);
+            }}
+        >
             <div>
                 <div className={'avatar size-8'}>
-                    <img className={'size-7 rounded-full'} src={faker.image.avatar()} alt={'profile'} />
+                    <img
+                        className={'size-7 rounded-full'}
+                        src={`${VITE_SERVER_URL}/image/${profileImagePath}/${profileImageName}`}
+                        alt={'profile'}
+                    />
                 </div>
             </div>
             <div className={'flex flex-1 flex-col gap-y-1'}>
                 <Link to={'/user/10'} className={'w-fit text-[0.95rem] font-bold underline underline-offset-4'}>
-                    <span>kkangasdf</span>
+                    <span>{nickname}</span>
                 </Link>
                 <Link to={`mailto:kkangasdf12@naver.com`} className={'w-fit text-[0.8rem] text-slate-500'}>
-                    kkangasdf12@naver.com
+                    {email}
                 </Link>
             </div>
             <div className={'flex flex-col justify-center'}>
