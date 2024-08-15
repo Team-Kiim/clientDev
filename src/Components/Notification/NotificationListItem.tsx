1c23bfc99d23ff877137e7e5b1374480affbd4c5
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-15 13:03:07 +0900

    feat: 개별 알림 삭제 버튼 추가

--- src/Components/Notification/NotificationListItem.tsx
+++ src/Components/Notification/NotificationListItem.tsx
@@ -2,29 +2,33 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/ko.js';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { faker } from '@faker-js/faker';
+import { HiOutlineTrash } from 'react-icons/hi2';
 
 dayjs.extend(relativeTime);
 
 export default function NotificationListItem() {
     return (
-        <li className={'flex h-20 w-full shrink-0 cursor-pointer rounded-lg bg-white'}>
-            <div className={'p-3'}>
-                <div className={'avatar'}>
-                    <div className={'size-8 rounded-full'}>
-                        <img className={'size-10 rounded-full'} src={faker.image.avatar()} alt={'profileImg'} />
-                    </div>
+        <li className={'flex h-20 w-full shrink-0 gap-x-3 rounded-lg bg-white p-3'}>
+            <div className={'avatar'}>
+                <div className={'size-8 rounded-full'}>
+                    <img className={'size-10 rounded-full'} src={faker.image.avatar()} alt={'profileImg'} />
                 </div>
             </div>
-            <div className={'flex shrink-0 grow basis-0 flex-col gap-y-1 p-3'}>
-                <div>
+            <div className={'flex-1 shrink-0 flex-grow basis-0'}>
+                <div className={'flex w-full flex-col gap-y-1'}>
                     <p className={'line-clamp-2 text-[0.8rem]'}>
                         <span className={'font-extrabold'}>kkangasdf1202</span>
                         님이 게시글에 댓글을 작성하였습니다.
                     </p>
+                    <div className={'text-[0.75rem] font-bold text-slate-400'}>
+                        <span>{dayjs('2024-7-10').fromNow(true)} 전</span>
+                    </div>
                 </div>
-                <div className={'text-[0.75rem] font-bold text-slate-400'}>
-                    <span>{dayjs('2024-7-10').fromNow(true)} 전</span>
-                </div>
+            </div>
+            <div>
+                <button className={'rounded-full p-1'} type={'button'}>
+                    <HiOutlineTrash className={'size-5 text-neutral-800'} />
+                </button>
             </div>
         </li>
     );
