e1877abff39b0a0c6a544aa68b03b3c3ead61f78
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-19 15:01:56 +0900

    remove: 사용되지 않는 파일 삭제

--- src/Pages/qnas/write/Utils/getQnAType.ts
+++
@@ -1,9 +0,0 @@
-const getQnAType = (searchParams: URLSearchParams): string => {
-    if (searchParams.get('qna_type') !== 'dev' && searchParams.get('qna_type') !== 'nonDev') {
-        return 'dev';
-    }
-
-    return searchParams.get('qna_type') ?? 'dev';
-};
-
-export default getQnAType;
