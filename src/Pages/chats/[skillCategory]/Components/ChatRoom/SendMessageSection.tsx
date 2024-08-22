1d3a0b323820f8b221789e35141f1723f646158a
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-22 13:39:16 +0900

    feat: 기술별 채팅방 내 소스 코드 전송 관련 UI 추가

--- src/Pages/chats/[skillCategory]/Components/ChatRoom/SendMessageSection.tsx
+++ src/Pages/chats/[skillCategory]/Components/ChatRoom/SendMessageSection.tsx
@@ -1,7 +1,14 @@
-import { KeyboardEventHandler, useEffect, useRef } from 'react';
+import { KeyboardEventHandler, useEffect, useRef, useState } from 'react';
 import { GoCodeSquare, GoImage } from 'react-icons/go';
+import SendCodeModal from '@/Components/ChatRelated/SendCode/SendCodeModal.tsx';
 
 export default function SendMessageSection() {
+    const [isSendCodeModalOpen, setIsSendCodeModalOpen] = useState(false);
+
+    const handleSendCodeButtonClick = () => {
+        setIsSendCodeModalOpen(true);
+    };
+
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
     const handleKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = event => {
@@ -26,26 +33,38 @@ export default function SendMessageSection() {
     }, []);
 
     return (
-        <div className={'flex items-center gap-x-2 border-t border-slate-200 px-3 py-3'}>
-            <div className={'flex items-center gap-x-2'}>
-                <button className={'rounded-full p-1 transition-all hover:bg-slate-100'} type={'button'}>
-                    <GoImage className={'size-6 text-slate-500'} />
-                </button>
-                <button className={'rounded-full p-1 transition-all hover:bg-slate-100'} type={'button'}>
-                    <GoCodeSquare className={'size-6 text-slate-500'} />
-                </button>
+        <>
+            <div className={'flex items-center gap-x-2 border-t border-slate-200 px-3 py-3'}>
+                <div className={'flex items-center gap-x-2'}>
+                    <button className={'rounded-full p-1 transition-all hover:bg-slate-100'} type={'button'}>
+                        <GoImage className={'size-6 text-slate-500'} />
+                    </button>
+                    <button
+                        className={'rounded-full p-1 transition-all hover:bg-slate-100'}
+                        type={'button'}
+                        onClick={handleSendCodeButtonClick}
+                    >
+                        <GoCodeSquare className={'size-6 text-slate-500'} />
+                    </button>
+                </div>
+                <div className={'flex flex-1 items-center'}>
+                    <textarea
+                        ref={textAreaRef}
+                        placeholder={'메시지를 입력하세요..'}
+                        className={
+                            'w-full resize-none rounded-xl border border-slate-200 px-3 py-1.5 text-[0.85rem] placeholder:text-slate-400 focus:outline-none'
+                        }
+                        onKeyDown={handleKeyDown}
+                        rows={1}
+                    />
+                </div>
             </div>
-            <div className={'flex flex-1 items-center'}>
-                <textarea
-                    ref={textAreaRef}
-                    placeholder={'메시지를 입력하세요..'}
-                    className={
-                        'w-full resize-none rounded-xl border border-slate-200 px-3 py-1.5 text-[0.85rem] placeholder:text-slate-400 focus:outline-none'
-                    }
-                    onKeyDown={handleKeyDown}
-                    rows={1}
-                />
-            </div>
-        </div>
+            <SendCodeModal
+                isSendCodeModalOpen={isSendCodeModalOpen}
+                closeSendCodeModal={() => {
+                    setIsSendCodeModalOpen(false);
+                }}
+            />
+        </>
     );
 }
