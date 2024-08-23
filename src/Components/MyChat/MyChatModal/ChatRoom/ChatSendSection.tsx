bcc93122c345f2719d3bee9d6968997760e80d0f
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-23 11:24:53 +0900

    design: 1:1 채팅방 내 메시지 전송 부분 디자인 수정

--- src/Components/MyChat/MyChatModal/ChatRoom/ChatSendSection.tsx
+++ src/Components/MyChat/MyChatModal/ChatRoom/ChatSendSection.tsx
@@ -1,5 +1,5 @@
 import { KeyboardEventHandler } from 'react';
-import { HiOutlineCodeBracket, HiOutlinePhoto } from 'react-icons/hi2';
+import { GoCodeSquare, GoImage } from 'react-icons/go';
 
 interface Props {
     chatRoomId: string | number;
@@ -22,20 +22,20 @@ export default function ChatSendSection({ chatRoomId }: Props) {
     };
 
     return (
-        <div className={'flex items-center gap-x-2 px-3'}>
+        <div className={'flex items-center gap-x-2 border-t border-slate-200 px-3 pt-2'}>
             <div className={'flex items-center gap-x-2'}>
                 <button className={'rounded-full p-1 hover:bg-slate-100'} type={'button'}>
-                    <HiOutlinePhoto className={'size-6'} />
+                    <GoImage className={'size-6 text-slate-500'} />
                 </button>
                 <button className={'rounded-full p-1 hover:bg-slate-100'}>
-                    <HiOutlineCodeBracket className={'size-6'} />
+                    <GoCodeSquare className={'size-6 text-slate-500'} />
                 </button>
             </div>
             <div className={'flex flex-1 items-center'}>
                 <textarea
                     placeholder={'메시지를 입력하세요..'}
                     className={
-                        'w-full resize-none rounded-3xl border border-slate-200 px-3 py-1.5 text-[0.85rem] focus:outline-none'
+                        'w-full resize-none rounded-xl border border-slate-200 px-4 py-1.5 text-[0.8rem] placeholder:text-slate-400 focus:outline-none'
                     }
                     onKeyDown={handleKeyDown}
                     rows={1}
