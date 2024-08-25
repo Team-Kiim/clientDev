23984aef051c635f68b3a04839300697456d1d5f
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-25 13:30:40 +0900

    feat: 사용자 게시글 검색 input 컴포넌트 추가

---
+++ src/Pages/user/posts/Components/PostSearchInput.tsx
@@ -0,0 +1,48 @@
+import { KeyboardEventHandler, useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
+
+export default function PostSearchInput() {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = event => {
+        if (event.nativeEvent.isComposing) {
+            return;
+        }
+
+        if (event.code === 'Enter') {
+            const postTitleToSearch = inputRef.current.value;
+            if (postTitleToSearch.trim().length !== 0) {
+                searchParams.set('post-title', postTitleToSearch);
+                setSearchParams(searchParams);
+            }
+        }
+    };
+
+    const handleSearchButtonClick = () => {
+        const postTitleToSearch = inputRef.current.value;
+        if (postTitleToSearch.trim().length !== 0) {
+            searchParams.set('post-title', postTitleToSearch);
+            setSearchParams(searchParams);
+        }
+    };
+
+    return (
+        <div className={'flex w-72 gap-x-2 rounded-lg border border-slate-200 px-3 py-2'}>
+            <input
+                className={'flex-1 text-[0.8rem] focus:outline-none'}
+                type={'text'}
+                onKeyDown={handleKeyDown}
+                placeholder={'게시글 제목 검색'}
+                autoComplete={'off'}
+                autoCapitalize={'none'}
+                ref={inputRef}
+            />
+            <button type={'button'} onClick={handleSearchButtonClick}>
+                <HiMagnifyingGlass className={'size-5 text-neutral-800'} />
+            </button>
+        </div>
+    );
+}
