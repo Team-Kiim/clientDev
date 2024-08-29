825eabd93c4cd36e054c4ca9e607f5201e7f324a
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-29 18:46:13 +0900

    feat: 계정 관리 페이지 내 이메일 변경 부분 추가

--- src/Pages/user/account/page.tsx
+++ src/Pages/user/account/page.tsx
@@ -1,19 +1,17 @@
-import EmailUpdateForm from '@/Pages/user/account/Components/EmailUpdate/EmailUpdateForm.tsx';
-import PasswordUpdateForm from '@/Pages/user/account/Components/PasswordUpdate/PasswordUpdateForm.tsx';
+import UpdateEmailForm from '@/Pages/user/account/Components/UpdateEmail/UpdateEmailForm.tsx';
 
 export default function Page() {
     return (
         <>
-            <div className={'mb-6 flex w-4/6 flex-col gap-y-6'}>
+            <div className={'mb-6 flex flex-col gap-y-6'}>
                 <h1 className={'flex text-xl font-extrabold'}>계정 관리</h1>
                 <div className={'flex flex-col gap-y-4'}>
                     <h3 className={'text-[0.95rem] font-bold'}>이메일 변경</h3>
-                    <EmailUpdateForm />
+                    <UpdateEmailForm />
                 </div>
                 <hr />
                 <div className={'flex flex-col gap-y-5'}>
                     <h3 className={'text-[0.95rem] font-bold'}>비밀번호 변경</h3>
-                    <PasswordUpdateForm />
                 </div>
             </div>
         </>
