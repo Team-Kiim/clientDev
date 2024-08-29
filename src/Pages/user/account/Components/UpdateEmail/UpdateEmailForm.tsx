3d6fd06faeaee3aae1201dcbf5294c5e82b65961
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-29 18:44:20 +0900

    feat: 이메일 변경 폼 컴포넌트 추가

---
+++ src/Pages/user/account/Components/UpdateEmail/UpdateEmailForm.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import 'react-toastify/dist/ReactToastify.css';
+import NewEmailField from '@/Pages/user/account/Components/UpdateEmail/NewEmailField.tsx';
+import VerificationCodeField from '@/Pages/user/account/Components/UpdateEmail/VerificationCodeField.tsx';
+
+interface FormValues {
+    newEmail: string;
+    verificationCode: string;
+}
+
+export default function UpdateEmailForm() {
+    const [isEmailVerified, setIsEmailVerified] = useState(false);
+
+    const [isRequestingVerification, setIsRequestingVerification] = useState(false);
+
+    const [isVerificationCodeSent, setIsVerificationCodeSent] = useState(false);
+
+    const formMethods = useForm<FormValues>({
+        mode: 'onBlur',
+        defaultValues: {
+            newEmail: '',
+            verificationCode: '',
+        },
+    });
+
+    const onSubmit: SubmitHandler<FormValues> = async data => {
+        const newEmail = data.newEmail.replace(/\s/gi, '');
+        console.log(newEmail);
+    };
+
+    return (
+        <FormProvider {...formMethods}>
+            <form className={'flex w-full flex-col gap-y-10'} onSubmit={formMethods.handleSubmit(onSubmit)}>
+                <div className={'flex w-full flex-col gap-y-4'}>
+                    <NewEmailField
+                        isRequestingVerification={isRequestingVerification}
+                        updateIsRequestingVerification={(isRequestingVerification: boolean) => {
+                            setIsRequestingVerification(isRequestingVerification);
+                        }}
+                        isVerificationCodeSent={isVerificationCodeSent}
+                        markAsVerificationCodeSent={() => {
+                            setIsVerificationCodeSent(true);
+                        }}
+                    />
+                    <VerificationCodeField
+                        isRequestingVerification={isRequestingVerification}
+                        isVerificationCodeSent={isVerificationCodeSent}
+                        isEmailVerified={isEmailVerified}
+                        markAsEmailVerified={() => {
+                            setIsEmailVerified(true);
+                        }}
+                    />
+                </div>
+                <div className={'flex w-full justify-end'}>
+                    <button
+                        className={
+                            'rounded-2xl bg-neutral-800 px-4 py-2 text-[0.85rem] font-bold text-white transition-all enabled:hover:bg-neutral-800/80  disabled:opacity-50'
+                        }
+                        type={'submit'}
+                        disabled={!isEmailVerified}
+                    >
+                        변경
+                    </button>
+                </div>
+            </form>
+        </FormProvider>
+    );
+}
