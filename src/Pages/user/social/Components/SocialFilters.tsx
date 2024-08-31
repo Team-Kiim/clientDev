b92c68805aa7d29ab4f7054e1154c9cc8febe47b
Kim Kangmin <rlarkdals1202@gmail.com>
2024-08-31 22:25:16 +0900

    feat: 팔로잉, 팔로워 필터 컴포넌트 추가

---
+++ src/Pages/user/social/Components/SocialFilters.tsx
@@ -0,0 +1,40 @@
+import { useSearchParams } from 'react-router-dom';
+
+const socialFilters = [
+    { value: 'following', label: '팔로잉' },
+    { value: 'follower', label: '팔로워' },
+];
+
+const getCurrentSocialType = (searchParams: URLSearchParams): string => {
+    const currentSocialType = searchParams.get('type');
+    return socialFilters.some(filter => filter.value === currentSocialType) ? currentSocialType : 'following';
+};
+
+export default function SocialFilters() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const currentSocialType = getCurrentSocialType(searchParams);
+
+    const handleFilterButtonClick = (filterValue: string) => {
+        searchParams.set('type', filterValue);
+        setSearchParams(searchParams);
+    };
+
+    return (
+        <ul className={'flex gap-x-2'}>
+            {socialFilters.map(filter => (
+                <li key={filter.value}>
+                    <button
+                        className={`px-3.5 py-1.5 text-[0.8rem] ${currentSocialType === filter.value ? 'border-neutral-800 bg-neutral-800 text-white' : 'border-slate-200 bg-white text-black hover:bg-slate-100'} rounded-3xl border font-bold transition-all`}
+                        onClick={() => {
+                            handleFilterButtonClick(filter.value);
+                        }}
+                        type={'button'}
+                    >
+                        {filter.label}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+}
