651a8028faa9c90d17704bc1a358e2cfb15f9ca8
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-01 15:57:31 +0900

    feat: 팔로잉, 팔로워 목록 요청 관련 에러 발생 시 보여줄 fallback 컴포넌트 추가

---
+++ src/Pages/user/social/Components/SocialApiErrorFallback.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { FallbackProps } from 'react-error-boundary';
+import { getCurrentSocialType } from '@/Pages/user/social/Utils/getCurrentSocialType.ts';
+
+export default function SocialApiErrorFallback({ resetErrorBoundary }: FallbackProps) {
+    const [searchParams] = useSearchParams();
+
+    const queryClient = useQueryClient();
+
+    const currentSocialType = getCurrentSocialType(searchParams);
+
+    useEffect(() => {
+        return () => {
+            queryClient.resetQueries({ queryKey: ['social', currentSocialType] }).catch();
+        };
+    }, []);
+
+    return (
+        <div className={'my-10 flex w-full flex-col items-center gap-y-4'}>
+            <p className={'text-sm text-slate-400'}>
+                {currentSocialType === 'following' ? '팔로잉' : '팔로워'} 목록을 불러올 수 없어요. 잠시 후 다시
+                시도해주세요.
+            </p>
+            <button
+                className={
+                    'rounded-3xl bg-neutral-800 px-3.5 py-2 text-[0.8rem] font-bold text-white transition-all hover:bg-neutral-800/80'
+                }
+                type={'button'}
+                onClick={resetErrorBoundary}
+            >
+                다시 시도
+            </button>
+        </div>
+    );
+}
