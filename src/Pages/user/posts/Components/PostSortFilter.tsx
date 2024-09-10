f34ef8d5a41f791c132b77377475e23df8926435
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-10 17:28:34 +0900

    refactor: getCurrentPostFilter 함수의 반환값으로 게시글 정렬 필터 사용

--- src/Pages/user/posts/Components/PostSortFilter.tsx
+++ src/Pages/user/posts/Components/PostSortFilter.tsx
@@ -1,10 +1,11 @@
 import { useSearchParams } from 'react-router-dom';
 import { ChangeEventHandler } from 'react';
+import getCurrentPostFilter from '@/Pages/user/posts/Utils/getCurrentPostFilter.tsx';
 
 export default function PostSortFilter() {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const currentPostSortFilter = searchParams.get('post-sort') ?? 'latest';
+    const { postSort } = getCurrentPostFilter(searchParams);
 
     const handleSelectChange: ChangeEventHandler<HTMLSelectElement> = event => {
         const postSortFilter = event.target.value;
@@ -14,13 +15,13 @@ export default function PostSortFilter() {
 
     return (
         <select className={'select select-sm w-44 font-bold'} onChange={handleSelectChange}>
-            <option value={'latest'} selected={currentPostSortFilter === 'latest'}>
+            <option value={'latest'} selected={postSort === 'latest'}>
                 최신순
             </option>
-            <option value={'view'} selected={currentPostSortFilter === 'view'}>
+            <option value={'view'} selected={postSort === 'view'}>
                 조회순
             </option>
-            <option value={'like'} selected={currentPostSortFilter === 'like'}>
+            <option value={'like'} selected={postSort === 'like'}>
                 좋아요순
             </option>
         </select>
