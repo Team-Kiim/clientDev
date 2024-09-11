7fe7ab7fa3b4aa49db48e17094a5d9f8455d42af
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-11 14:22:34 +0900

    design: 다시 시도 버튼 디자인 수정

--- src/Pages/user/social/Components/SocialApiErrorFallback.tsx
+++ src/Pages/user/social/Components/SocialApiErrorFallback.tsx
@@ -25,7 +25,7 @@ export default function SocialApiErrorFallback({ resetErrorBoundary }: FallbackP
             </p>
             <button
                 className={
-                    'rounded-3xl bg-neutral-800 px-3.5 py-2 text-[0.8rem] font-bold text-white transition-all hover:bg-neutral-800/80'
+                    'rounded-3xl bg-gradient-to-br from-violet-600 to-fuchsia-600 px-3.5 py-2 text-[0.8rem] font-bold text-white transition-all hover:scale-105'
                 }
                 type={'button'}
                 onClick={resetErrorBoundary}
