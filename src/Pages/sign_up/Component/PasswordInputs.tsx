13610696304eb75bef4f276f15296d278b639566
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-15 20:36:26 +0900

    design: 비밀번호, confirm 비밀번호 입력 필드 디자인 수정

--- src/Pages/sign_up/Component/PasswordInputs.tsx
+++ src/Pages/sign_up/Component/PasswordInputs.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
-import { HiOutlineEye, HiOutlineEyeSlash, HiOutlineLockClosed, HiOutlineExclamationTriangle } from 'react-icons/hi2';
+import { HiOutlineEye, HiOutlineEyeSlash, HiOutlineLockClosed, HiOutlineExclamationCircle } from 'react-icons/hi2';
 import { passwordRegexp } from '@/Constants/regexps.ts';
 
 interface FormData {
@@ -21,13 +21,13 @@ export default function PasswordInputs() {
             <div className={'flex w-full flex-col gap-y-2'}>
                 <div
                     className={
-                        'flex items-center gap-x-2 rounded-xl border border-slate-300 px-2 py-3.5 focus-within:border-violet-700'
+                        'flex items-center gap-x-2 rounded-2xl border border-slate-300 px-3 py-3.5 transition-all focus-within:border-violet-500'
                     }
                 >
-                    <HiOutlineLockClosed className={'size-6 text-slate-500'} />
+                    <HiOutlineLockClosed className={'size-5 text-slate-800'} />
                     <input
                         className={
-                            'w-full flex-1 text-[0.9rem] focus:outline-none disabled:bg-white disabled:opacity-75'
+                            'w-full flex-1 text-[0.9rem] placeholder:text-slate-400 focus:outline-none disabled:bg-white disabled:opacity-75'
                         }
                         placeholder={'비밀번호'}
                         type={isPasswordVisible ? 'text' : 'password'}
@@ -51,21 +51,21 @@ export default function PasswordInputs() {
                         }}
                     >
                         {isPasswordVisible ? (
-                            <HiOutlineEyeSlash className={'size-6 text-slate-500'} />
+                            <HiOutlineEyeSlash className={'size-5 text-slate-800'} />
                         ) : (
-                            <HiOutlineEye className={'size-6 text-slate-500'} />
+                            <HiOutlineEye className={'size-5 text-slate-800'} />
                         )}
                     </button>
                 </div>
                 {errors?.password?.message && errors?.password.type === 'required' && (
-                    <div className={'m-0.5 flex items-center gap-x-1 text-red-500 '}>
-                        <HiOutlineExclamationTriangle className={'size-5'} />
+                    <div className={'m-0.5 flex items-center gap-x-1 text-rose-500 '}>
+                        <HiOutlineExclamationCircle className={'size-5'} />
                         <p className={'text-[0.8rem]'}>{errors.password.message}</p>
                     </div>
                 )}
                 {errors?.password?.message && errors?.password.type === 'pattern' && (
-                    <div className={'m-0.5 flex items-center gap-x-1 text-red-500 '}>
-                        <HiOutlineExclamationTriangle className={'size-5'} />
+                    <div className={'m-0.5 flex items-center gap-x-1 text-rose-500 '}>
+                        <HiOutlineExclamationCircle className={'size-5'} />
                         <p className={'text-[0.8rem]'}>{errors.password.message}</p>
                     </div>
                 )}
@@ -73,13 +73,13 @@ export default function PasswordInputs() {
             <div className={'flex w-full flex-col gap-y-2'}>
                 <div
                     className={
-                        'flex items-center gap-x-2 rounded-xl border border-slate-300 px-2 py-3.5 focus-within:border-violet-700'
+                        'flex items-center gap-x-2 rounded-2xl border border-slate-300 px-3 py-3.5 transition-all focus-within:border-violet-500'
                     }
                 >
-                    <HiOutlineLockClosed className={'size-6 text-slate-500'} />
+                    <HiOutlineLockClosed className={'size-5 text-slate-800'} />
                     <input
                         className={
-                            'w-full flex-1 text-[0.9rem] focus:outline-none disabled:bg-white disabled:opacity-75'
+                            'w-full flex-1 text-[0.9rem] placeholder:text-slate-400 focus:outline-none disabled:bg-white disabled:opacity-75'
                         }
                         placeholder={'비밀번호 확인'}
                         type={'password'}
@@ -100,14 +100,14 @@ export default function PasswordInputs() {
                     />
                 </div>
                 {errors?.confirmPassword?.message && errors?.confirmPassword.type === 'required' && (
-                    <div className={'m-0.5 flex items-center gap-x-1 text-red-500 '}>
-                        <HiOutlineExclamationTriangle className={'size-5'} />
+                    <div className={'m-0.5 flex items-center gap-x-1 text-rose-500 '}>
+                        <HiOutlineExclamationCircle className={'size-5'} />
                         <p className={'text-[0.8rem]'}>{errors.confirmPassword.message}</p>
                     </div>
                 )}
                 {errors?.confirmPassword?.message && errors?.confirmPassword.type === 'isNotMatched' && (
-                    <div className={'m-0.5 flex items-center gap-x-1 text-red-500 '}>
-                        <HiOutlineExclamationTriangle className={'size-5'} />
+                    <div className={'m-0.5 flex items-center gap-x-1 text-rose-500 '}>
+                        <HiOutlineExclamationCircle className={'size-5'} />
                         <p className={'text-[0.8rem]'}>{errors.confirmPassword.message}</p>
                     </div>
                 )}
