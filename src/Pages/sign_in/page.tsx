62ede2a7b936256991a6a310035603a363bff186
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-15 20:28:04 +0900

    design: 로그인 페이지 디자인 수정

--- src/Pages/sign_in/page.tsx
+++ src/Pages/sign_in/page.tsx
@@ -6,21 +6,29 @@ import OAuthSection from '@/Components/Auth/OAuthSection/OAuthSection.tsx';
 export default function Page() {
     return (
         <div className={'mx-auto my-24 flex w-[22rem] flex-col items-center'}>
-            <div className={'mb-1.5 flex w-full flex-col items-center gap-y-5'}>
+            <div className={'mb-1.5 flex w-full flex-col items-center gap-y-7'}>
                 <div className={'flex w-full flex-col items-center gap-y-10'}>
-                    <Link className={'logo text-4xl text-violet-700'} to={'/'}>
+                    <Link
+                        className={
+                            'logo inline-block bg-gradient-to-br from-violet-500 to-indigo-500 bg-clip-text text-4xl font-bold text-transparent'
+                        }
+                        to={'/'}
+                    >
                         KoffeeChat
                     </Link>
                     <SignInForm />
                 </div>
                 <div className={'flex w-full justify-between px-1'}>
                     <PasswordResetSection />
-                    <Link className={'text-[0.85rem] font-bold text-violet-700 transition-all'} to={'/sign_up'}>
+                    <Link
+                        className={'text-[0.8rem] font-bold text-violet-500 hover:underline hover:underline-offset-4'}
+                        to={'/sign_up'}
+                    >
                         회원가입
                     </Link>
                 </div>
             </div>
-            <div className={'divider text-[0.8rem] text-gray-600'}>소셜로 로그인</div>
+            <div className={'divider py-5 text-[0.8rem] text-gray-600'}>소셜로 로그인</div>
             <OAuthSection />
         </div>
     );
