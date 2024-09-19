724dc24f658aaf8b4aeed71e9de5c49c9fb80563
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-19 17:33:16 +0900

    design: 로그인 페이지 디자인 수정

--- src/Pages/sign_in/page.tsx
+++ src/Pages/sign_in/page.tsx
@@ -6,11 +6,11 @@ import OAuthSection from '@/Components/Auth/OAuthSection/OAuthSection.tsx';
 export default function Page() {
     return (
         <div className={'mx-auto my-24 flex w-[22rem] flex-col items-center'}>
-            <div className={'mb-1.5 flex w-full flex-col items-center gap-y-7'}>
+            <div className={'mb-1.5 flex w-full flex-col items-center gap-y-6'}>
                 <div className={'flex w-full flex-col items-center gap-y-10'}>
                     <Link
                         className={
-                            'logo inline-block bg-gradient-to-br from-violet-500 to-indigo-500 bg-clip-text text-4xl font-bold text-transparent'
+                            'logo inline-block bg-gradient-to-br from-plump-purple-500 to-plump-purple-700 bg-clip-text text-4xl font-bold text-transparent'
                         }
                         to={'/'}
                     >
@@ -18,14 +18,19 @@ export default function Page() {
                     </Link>
                     <SignInForm />
                 </div>
-                <div className={'flex w-full justify-between px-1'}>
-                    <PasswordResetSection />
-                    <Link
-                        className={'text-[0.8rem] font-bold text-violet-500 hover:underline hover:underline-offset-4'}
-                        to={'/sign_up'}
-                    >
-                        회원가입
-                    </Link>
+                <div className={'flex w-full flex-col justify-between gap-y-3 px-1'}>
+                    <div className={'flex justify-end'}>
+                        <PasswordResetSection />
+                    </div>
+                    <p className={'text-[0.8rem] text-slate-500'}>
+                        아직 회원이 아니신가요? {''}
+                        <Link
+                            className={'font-bold text-plump-purple-600 hover:underline hover:underline-offset-4'}
+                            to={'/sign_up'}
+                        >
+                            이메일로 가입하기
+                        </Link>
+                    </p>
                 </div>
             </div>
             <div className={'divider py-5 text-[0.8rem] text-gray-600'}>소셜로 로그인</div>
