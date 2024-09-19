575ba20d2c7abeac13cfe7fb35a1d19a6a74adef
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-19 17:33:57 +0900

    design: 회원가입 페이지 디자인 수정

--- src/Pages/sign_up/page.tsx
+++ src/Pages/sign_up/page.tsx
@@ -5,17 +5,28 @@ import OAuthSection from '@/Components/Auth/OAuthSection/OAuthSection.tsx';
 export default function Page() {
     return (
         <div className={'mx-auto my-24 flex w-[22rem] flex-col items-center'}>
-            <div className={'mb-1.5 flex w-full flex-col items-center gap-y-5'}>
+            <div className={'mb-1.5 flex w-full flex-col items-center gap-y-6'}>
                 <div className={'flex w-full flex-col items-center gap-y-10'}>
                     <Link
                         className={
-                            'logo inline-block bg-gradient-to-br from-violet-500 to-indigo-500 bg-clip-text text-4xl font-bold text-transparent'
+                            'logo inline-block bg-gradient-to-br from-plump-purple-500 to-plump-purple-700 bg-clip-text text-4xl font-bold text-transparent'
                         }
                         to={'/'}
                     >
                         KoffeeChat
                     </Link>
                     <SignUpForm />
+                </div>
+                <div className={'flex w-full flex-col px-1'}>
+                    <p className={'text-[0.8rem] text-slate-500'}>
+                        이미 회원이신가요? {''}
+                        <Link
+                            className={'font-bold text-plump-purple-600 hover:underline hover:underline-offset-4'}
+                            to={'/sign_in'}
+                        >
+                            이메일로 로그인하기
+                        </Link>
+                    </p>
                 </div>
             </div>
             <div className={'divider text-[0.8rem] text-gray-600'}>소셜로 회원가입</div>
