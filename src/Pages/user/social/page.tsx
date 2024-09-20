ac9cab24752b5399f32bddb16bfc5700eb57507d
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-20 14:21:04 +0900

    feat: isLoginMember prop 값에 따라 다른 텍스트 보여주도록 변경

--- src/Pages/user/social/page.tsx
+++ src/Pages/user/social/page.tsx
@@ -4,11 +4,19 @@ import SocialFilters from '@/Pages/user/social/Components/SocialFilters.tsx';
 import SocialMediaUserList from '@/Pages/user/social/Components/SocialMediaUserList.tsx';
 import SocialMediaUserListLoading from '@/Pages/user/social/Components/SocialMediaUserListLoading.tsx';
 import SocialApiErrorBoundary from '@/Pages/user/social/Components/SocialApiErrorBoundary.tsx';
+import type { User } from '@/Types/User.ts';
+import { useOutletContext } from 'react-router-dom';
+
+interface UserDataContext {
+    userData: User;
+}
 
 export default function Page() {
+    const { userData } = useOutletContext<UserDataContext>();
+
     return (
         <div className={'mb-6 flex flex-col gap-y-5'}>
-            <h1 className={'flex text-xl font-extrabold'}>소셜 관리</h1>
+            <h1 className={'flex text-xl font-extrabold'}>{userData.isLoginMember ? '소셜 관리' : '소셜 정보'}</h1>
             <div className={'flex flex-col gap-y-10'}>
                 <div className={'flex items-center justify-between'}>
                     <SocialFilters />
