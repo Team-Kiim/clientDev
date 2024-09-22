d74d6db962aa5cce55c0a533a7c3950931e45d52
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-22 14:33:58 +0900

    design: QnA, 커뮤니티 게시글 목록 로딩 시 보여줄 로딩 컴포넌트 디자인 수정

--- src/Components/Post/PostListLoading.tsx
+++ src/Components/Post/PostListLoading.tsx
@@ -6,7 +6,7 @@ export default function PostListLoading() {
             <ul className={'grid grid-cols-4 gap-x-4 gap-y-10 px-6 pb-10 pt-3'}>
                 {Array.from(Array(8).keys()).map(e => (
                     <li key={e} className={'flex h-[21rem] flex-col gap-y-3 rounded-3xl shadow-lg transition-all'}>
-                        <Skeleton className={'h-36 w-full rounded-3xl'} />
+                        <Skeleton className={'h-36 w-full rounded-t-3xl'} />
                         <div className={'flex items-center gap-x-2 px-2'}>
                             <Skeleton className={'size-8'} circle />
                             <div className={'flex flex-col gap-y-1'}>
@@ -23,8 +23,8 @@ export default function PostListLoading() {
                             <Skeleton className={'h-2.5'} containerClassName={'h-2.5 flex'} />
                         </div>
                         <div className={'flex items-center justify-end gap-x-2 px-4 pb-2'}>
-                            <Skeleton className={'h-[1.05rem] w-9'} />
-                            <Skeleton className={'h-[1.05rem] w-9'} />
+                            <Skeleton className={'h-[1.05rem] w-[3.25rem]'} />
+                            <Skeleton className={'h-[1.05rem] w-[3.25rem]'} />
                         </div>
                     </li>
                 ))}
