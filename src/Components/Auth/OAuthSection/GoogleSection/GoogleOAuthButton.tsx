eb0d59778d86c2a53255ddcfc40574246f08792e
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-25 15:40:49 +0900

    fix: <g> 요소의 stroke-width, fill-rule 어트리뷰트 이름 수정

--- src/Components/Auth/OAuthSection/GoogleSection/GoogleOAuthButton.tsx
+++ src/Components/Auth/OAuthSection/GoogleSection/GoogleOAuthButton.tsx
@@ -24,7 +24,7 @@ export default function GoogleOAuthButton() {
                 <title>Google-color</title>
                 <desc>Created with Sketch.</desc>
                 <defs></defs>
-                <g id='Icons' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
+                <g id='Icons' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
                     <g id='Color-' transform='translate(-401.000000, -860.000000)'>
                         <g id='Google' transform='translate(401.000000, 860.000000)'>
                             <path
