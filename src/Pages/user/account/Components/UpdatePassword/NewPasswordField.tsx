eacb92ddd38cdaf4d05c60c246f52cafb7cb12c8
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-27 13:20:56 +0900

    design: 새 비밀번호 입력 필드 디자인 수정

--- src/Pages/user/account/Components/UpdatePassword/NewPasswordField.tsx
+++ src/Pages/user/account/Components/UpdatePassword/NewPasswordField.tsx
@@ -1,6 +1,6 @@
 import { useReducer } from 'react';
 import { useFormContext } from 'react-hook-form';
-import { HiOutlineExclamationTriangle, HiOutlineEye, HiOutlineEyeSlash, HiOutlineLockClosed } from 'react-icons/hi2';
+import { HiOutlineExclamationCircle, HiOutlineEye, HiOutlineEyeSlash, HiOutlineLockClosed } from 'react-icons/hi2';
 import { passwordRegexp } from '@/Constants/regexps.ts';
 
 interface Props {
@@ -23,7 +23,7 @@ export default function NewPasswordField({ isPasswordVerified }: Props) {
             <div className={'flex w-1/2 flex-col gap-y-2'}>
                 <label className={'w-fit text-[0.9rem] font-bold text-neutral-800'} htmlFor={'newPasswordInput'}>
                     새 비밀번호
-                    <span className={'text-red-500'}>﹡</span>
+                    <span className={'text-rose-500'}>﹡</span>
                 </label>
             </div>
             <div className={'flex w-full gap-x-3'}>
@@ -70,14 +70,14 @@ export default function NewPasswordField({ isPasswordVerified }: Props) {
                         </button>
                     </div>
                     {errors?.newPassword?.message && errors?.newPassword.type === 'required' && (
-                        <div className={'mx-1 flex items-center gap-x-1 text-red-500'}>
-                            <HiOutlineExclamationTriangle className={'size-4'} />
+                        <div className={'mx-1 flex items-center gap-x-1 text-rose-500'}>
+                            <HiOutlineExclamationCircle className={'size-4'} />
                             <p className={'text-[0.8rem]'}>{errors.newPassword.message}</p>
                         </div>
                     )}
                     {errors?.newPassword?.message && errors?.newPassword.type === 'pattern' && (
-                        <div className={'mx-1 flex items-center gap-x-1 text-red-500'}>
-                            <HiOutlineExclamationTriangle className={'size-4'} />
+                        <div className={'mx-1 flex items-center gap-x-1 text-rose-500'}>
+                            <HiOutlineExclamationCircle className={'size-4'} />
                             <p className={'text-[0.8rem]'}>{errors.newPassword.message}</p>
                         </div>
                     )}
