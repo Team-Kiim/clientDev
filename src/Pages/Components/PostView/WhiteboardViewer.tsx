8ea656b4aa7bbfdf1593ce5368436ca8c95ced47
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-27 18:51:03 +0900

    feat: QnA 게시글에서 보여줄 시각자료 컴포넌트 추가

---
+++ src/Pages/Components/PostView/WhiteboardViewer.tsx
@@ -0,0 +1,38 @@
+import { Excalidraw, MainMenu } from '@excalidraw/excalidraw';
+import { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
+
+interface Props {
+    visualData: object;
+}
+
+export default function WhiteboardViewer({ visualData }: Props) {
+    return (
+        <div className={'h-[34rem] rounded-xl border border-slate-200 p-2 shadow-xl'}>
+            <Excalidraw
+                langCode={'ko-KR'}
+                initialData={{
+                    elements: visualData as ExcalidrawElement[],
+                    scrollToContent: true,
+                }}
+                viewModeEnabled={true}
+                onScrollChange={() => {
+                    if (document.querySelector('.scroll-back-to-content')) {
+                        (document.querySelector('.scroll-back-to-content') as HTMLButtonElement).type = 'button';
+                    }
+                }}
+            >
+                <MainMenu>
+                    <MainMenu.Item onSelect={() => {}}>
+                        <a
+                            className={'!text-plump-purple-600'}
+                            href={'https://github.com/excalidraw/excalidraw'}
+                            target={'_blank'}
+                        >
+                            Excalidraw 깃허브
+                        </a>
+                    </MainMenu.Item>
+                </MainMenu>
+            </Excalidraw>
+        </div>
+    );
+}
