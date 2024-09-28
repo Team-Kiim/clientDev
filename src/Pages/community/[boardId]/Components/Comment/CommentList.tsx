b06790274615aa92509caa7717808dee4b30262c
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-28 17:16:24 +0900

    feat: 댓글 삭제 경고 알림창 변경

--- src/Pages/community/[boardId]/Components/Comment/CommentList.tsx
+++ src/Pages/community/[boardId]/Components/Comment/CommentList.tsx
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { useState } from 'react';
 import CommentListItem from '@/Pages/community/[boardId]/Components/Comment/CommentListItem.tsx';
 import { useDeleteComment } from '@/Pages/community/[boardId]/Components/Comment/Hooks/useDeleteComment.tsx';
@@ -30,9 +31,23 @@ export default function CommentList({ postId, commentInfoDtoList }: Props) {
     const { mutate } = useDeleteComment(postId);
 
     const handleCommentDeleteButtonClick = async (commentId: number) => {
-        if (window.confirm('정말로 댓글을 삭제하시겠습니까?')) {
-            mutate(commentId);
-        }
+        Swal.fire({
+            icon: 'warning',
+            text: '정말로 댓글을 삭제하시겠습니까?',
+            showCancelButton: true,
+            confirmButtonText: '확인',
+            cancelButtonText: '취소',
+            customClass: {
+                cancelButton: 'text-black font-bold bg-slate-100 rounded-xl text-[0.9rem]',
+                confirmButton: 'font-bold bg-plump-purple-600 rounded-xl text-[0.9rem]',
+                popup: 'rounded-[2rem]',
+                htmlContainer: '!text-slate-500',
+            },
+        }).then(result => {
+            if (result.isConfirmed) {
+                mutate(commentId);
+            }
+        });
     };
 
     return (
