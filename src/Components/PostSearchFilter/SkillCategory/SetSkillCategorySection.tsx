12f4eab4435aea02a546a9cdfb6b3baba32a2bf1
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-29 14:01:17 +0900

    feat: 카테고리 설정 부분 클릭해도 카테고리 설정 모달 열리도록 변경

--- src/Components/PostSearchFilter/SkillCategory/SetSkillCategorySection.tsx
+++ src/Components/PostSearchFilter/SkillCategory/SetSkillCategorySection.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { MdOutlineTag } from 'react-icons/md';
 import { VscSettings } from 'react-icons/vsc';
 import SetSkillCategoryModal from '@/Components/PostSearchFilter/SkillCategory/SetSkillCategoryModal.tsx';
 import SelectedCategoryList from '@/Components/PostSearchFilter/SkillCategory/SelectedCategoryList.tsx';
@@ -10,28 +9,36 @@ export default function SetSkillCategorySection() {
 
     const [searchParams] = useSearchParams();
 
+    const skillCategoryList = searchParams.getAll('category');
+
     return (
         <>
-            <div className={'flex w-[30rem] items-center gap-x-2 rounded-3xl border border-slate-200 px-3'}>
-                <MdOutlineTag className={'size-7'} style={{ fill: 'url(#icon-gradient)' }} />
-                {searchParams.getAll('category').length !== 0 ? (
+            <div
+                className={`flex w-3/5 flex-shrink-0 ${skillCategoryList.length === 0 ? 'cursor-pointer' : 'cursor-auto'} items-center gap-x-2 rounded-lg border border-slate-200 px-3`}
+                onClick={() => {
+                    if (skillCategoryList.length === 0) {
+                        setIsSetSkillCategoryModalOpen(!isSetSkillCategoryModalOpen);
+                    }
+                }}
+            >
+                <div className={'tooltip tooltip-bottom flex items-center'} data-tip={'카테고리 설정'}>
+                    <button
+                        type={'button'}
+                        onClick={event => {
+                            event.preventDefault();
+                            setIsSetSkillCategoryModalOpen(!isSetSkillCategoryModalOpen);
+                        }}
+                    >
+                        <VscSettings className={'size-7 text-plump-purple-600'} />
+                    </button>
+                </div>
+                {skillCategoryList.length !== 0 ? (
                     <SelectedCategoryList />
                 ) : (
                     <div className={'flex h-11 flex-1 items-center'}>
                         <p className={'text-[0.8rem] text-slate-400'}>카테고리를 설정해보세요.</p>
                     </div>
                 )}
-                <div className={'tooltip tooltip-bottom flex items-center'} data-tip={'카테고리 설정'}>
-                    <button
-                        className={'transition-all hover:scale-105'}
-                        type={'button'}
-                        onClick={() => {
-                            setIsSetSkillCategoryModalOpen(!isSetSkillCategoryModalOpen);
-                        }}
-                    >
-                        <VscSettings className={'size-7'} style={{ fill: 'url(#icon-gradient)' }} />
-                    </button>
-                </div>
             </div>
             <SetSkillCategoryModal
                 isModalOpen={isSetSkillCategoryModalOpen}
