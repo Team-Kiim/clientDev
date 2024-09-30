795d6390f3f026966035c1aa8b730e33a75d51f6
Kim Kangmin <rlarkdals1202@gmail.com>
2024-09-30 13:34:44 +0900

    design: QnA 게시글 카테고리 리스트 디자인 수정

--- src/Pages/qnas/[boardId]/Components/PostSkillCategoryList.tsx
+++ src/Pages/qnas/[boardId]/Components/PostSkillCategoryList.tsx
@@ -1,3 +1,4 @@
+import { VscSettings } from 'react-icons/vsc';
 import { CATEGORIES } from '@/Constants/categories.ts';
 
 interface Props {
@@ -10,18 +11,25 @@ interface Props {
 export default function PostSkillCategoryList({ categories }: Props) {
     const childSkillCategories = CATEGORIES.map(category => category.childCategories).flat();
     return (
-        <ul className={'mb-5 mt-7 flex items-center gap-x-2.5'}>
-            {categories.map(category => {
-                return (
-                    <li className={'rounded-xl bg-gray-100 px-3 py-2 text-[0.8rem] font-bold text-gray-700'}>
+        <div className={'flex w-full items-center gap-x-2'}>
+            <div className={'tooltip tooltip-bottom flex items-center'} data-tip={'카테고리'}>
+                <VscSettings className={'size-6 text-slate-800'} />
+            </div>
+
+            <ul className={'flex min-w-0 flex-1 flex-wrap gap-2'}>
+                {categories.map(category => (
+                    <li
+                        key={category.childSkillCategory}
+                        className={'rounded-xl bg-slate-100 px-3 py-2 text-[0.8rem] font-bold text-slate-800'}
+                    >
                         {
                             childSkillCategories.find(
                                 childSkillCategory => childSkillCategory.value === category.childSkillCategory,
                             ).label
                         }
                     </li>
-                );
-            })}
-        </ul>
+                ))}
+            </ul>
+        </div>
     );
 }
