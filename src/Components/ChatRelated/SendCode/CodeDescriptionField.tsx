51473ffea0ca3643db4f64067fc030b5675122d7
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-05 18:59:46 +0900

    Design: 코드 설명 입력 필드 디자인 수정

--- src/Components/ChatRelated/SendCode/CodeDescriptionField.tsx
+++ src/Components/ChatRelated/SendCode/CodeDescriptionField.tsx
@@ -13,11 +13,13 @@ export default function CodeDescriptionField() {
                 코드 설명
                 <span className={'text-red-500'}>﹡</span>
             </label>
-            <div className={'h-[30rem] w-full rounded-lg border border-slate-200'}>
+            <div
+                className={
+                    'h-[25rem] w-full rounded-2xl border border-slate-300 transition-all focus-within:border-plump-purple-700'
+                }
+            >
                 <textarea
-                    className={
-                        'h-full w-full resize-none overflow-y-auto rounded-lg p-3 text-[0.9rem] focus:outline-none'
-                    }
+                    className={'h-full w-full resize-none overflow-y-auto rounded-2xl p-3 text-sm focus:outline-none'}
                     placeholder={'코드에 대해서 설명해주세요.'}
                     {...register('codeDescription', {
                         required: true,
