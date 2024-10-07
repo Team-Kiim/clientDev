c84ac68b3fa41d8aa074b687749a991606848257
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-07 18:29:19 +0900

    Feat: 알림창 스타일 관련 객체 상수 추가

---
+++ src/Constants/alertStyle.ts
@@ -0,0 +1,12 @@
+import { SweetAlertCustomClass } from 'sweetalert2';
+
+const ALERT_STYLE: SweetAlertCustomClass = {
+    cancelButton: 'text-black font-bold bg-slate-100 rounded-xl text-sm',
+    confirmButton: 'font-bold bg-plump-purple-600 rounded-xl text-sm',
+    popup: 'rounded-xl pt-8 pb-5 px-4 w-[24rem] !flex !flex-col gap-y-1',
+    htmlContainer: '!text-slate-500  text-start !px-0 !text-start !text-sm',
+    actions: 'w-full justify-end my-0',
+    title: 'text-start font-extrabold text-slate-800 text-[1.2rem] p-0',
+};
+
+export default ALERT_STYLE;
