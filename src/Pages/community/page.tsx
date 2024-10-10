a5107cda1415b963a7ff85476498975cbdcc42f3
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-10 18:18:45 +0900

    Feat: 커뮤니티 게시글 목록 페이지 완성

--- src/Pages/community/page.tsx
+++ src/Pages/community/page.tsx
@@ -1,16 +1,20 @@
 import { Suspense } from 'react';
 import PostSortFilters from '@/Components/PostSearchFilter/PostSortFilters.tsx';
-import WritePostButton from '@/Components/Post/WritePostButton.tsx';
 import PostListLoading from '@/Components/Post/PostListLoading.tsx';
 import CommunityPostList from '@/Pages/community/Components/PostList/CommunityPostList.tsx';
+import SetPostHashtagSection from '@/Components/PostSearchFilter/PostHashtag/SetPostHashtagSection.tsx';
 
 export default function Page() {
     return (
         <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
             <div className={'w-[73rem]'}>
-                <div className={'flex items-center justify-between px-6'}>
-                    <PostSortFilters />
-                    <WritePostButton postType={'community'} />
+                <div className={'flex flex-col gap-y-5 px-6'}>
+                    <div className={'w-fit'}>
+                        <PostSortFilters />
+                    </div>
+                    <div>
+                        <SetPostHashtagSection />
+                    </div>
                 </div>
                 <main className={'mb-12 mt-2'}>
                     <Suspense fallback={<PostListLoading />}>
