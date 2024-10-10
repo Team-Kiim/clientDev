d092223e1a61bedc0787992aa5caf6ff1f899abb
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-10 18:20:03 +0900

    Feat: 메인 페이지 완성

--- src/Pages/page.tsx
+++ src/Pages/page.tsx
@@ -1,21 +1,21 @@
 import { Suspense } from 'react';
 import SetSkillCategorySection from '@/Components/PostSearchFilter/SkillCategory/SetSkillCategorySection.tsx';
 import PostSortFilters from '@/Components/PostSearchFilter/PostSortFilters.tsx';
-import WritePostButton from '@/Components/Post/WritePostButton.tsx';
 import PostListLoading from '@/Components/Post/PostListLoading.tsx';
 import QnAPostList from '@/Pages/qnas/Components/PostList/QnAPostList.tsx';
+import SetPostHashtagSection from '@/Components/PostSearchFilter/PostHashtag/SetPostHashtagSection.tsx';
 
 export default function Page() {
     return (
         <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
             <div className={'w-[73rem]'}>
-                <div className={'flex flex-col gap-y-5'}>
-                    <div className={'px-6'}>
-                        <SetSkillCategorySection />
-                    </div>
-                    <div className={'flex items-center justify-between px-6'}>
+                <div className={'flex flex-col gap-y-5 px-6'}>
+                    <div className={'flex items-end justify-between gap-x-4'}>
                         <PostSortFilters />
-                        <WritePostButton postType={'qnas'} />
+                    </div>
+                    <div className={'flex flex-col gap-y-2'}>
+                        <SetSkillCategorySection />
+                        <SetPostHashtagSection />
                     </div>
                 </div>
                 <main className={'mb-12 mt-2'}>
