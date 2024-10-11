2e984b144b3a142b2d41b80aac8c744650218866
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-11 14:21:47 +0900

    Feat: 도메인 추가 요청 부분 추가

--- src/Pages/user/Components/EmployeeVerification/EmployeeVerificationModal.tsx
+++ src/Pages/user/Components/EmployeeVerification/EmployeeVerificationModal.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { HiXMark } from 'react-icons/hi2';
 import SetCorpInfoSection from '@/Pages/user/Components/EmployeeVerification/SetCorpInfo/SetCorpInfoSection.tsx';
 import EmployeeVerificationForm from '@/Pages/user/Components/EmployeeVerification/EmployeeVerificationForm/EmployeeVerificationForm.tsx';
+import RequestToAddDomainView from '@/Pages/user/Components/EmployeeVerification/RequestToAddDomain/RequestToAddDomainView.tsx';
 import type CorpInfo from '@/Types/corpInfo.ts';
 
 interface Props {
@@ -48,43 +49,57 @@ export default function EmployeeVerificationModal({ isModalOpen, closeModal }: P
                     corpName: '',
                     corpEmailDomain: '',
                 });
+                setCurrentViewName('employeeVerificationView');
             }}
         >
-            <div className={'flex justify-end'}>
-                <button
-                    className={'rounded-full p-1.5 transition-all hover:bg-slate-100'}
-                    type={'button'}
-                    onClick={() => {
-                        closeModal();
-                    }}
-                >
-                    <HiXMark className={'size-7'} />
-                </button>
-            </div>
-            <div className={'flex flex-col gap-y-8'}>
-                <h1 className={'mx-2 text-lg font-extrabold'}>현직자 인증</h1>
-                <SetCorpInfoSection
-                    selectedCorpInfo={selectedCorpInfo}
-                    updateSelectedCorpInfo={updateSelectedCorpInfo}
-                />
-                <div className={'flex justify-end'}>
-                    <p className={'text-[0.75rem] text-slate-500'}>
-                        찾으시는 도메인이 없으신가요?{' '}
+            {currentViewName === 'employeeVerificationView' ? (
+                <>
+                    <div className={'flex justify-end'}>
                         <button
-                            className={'font-bold text-plump-purple-600 hover:underline hover:underline-offset-4'}
+                            className={'rounded-full p-1.5 transition-all hover:bg-slate-100'}
                             type={'button'}
                             onClick={() => {
-                                setCurrentViewName('requestToAddDomainView');
+                                closeModal();
                             }}
                         >
-                            도메인 추가 요청
+                            <HiXMark className={'size-7'} />
                         </button>
-                    </p>
-                </div>
-                {isCorpInfoSelected && (
-                    <EmployeeVerificationForm selectedCorpInfo={selectedCorpInfo} closeModal={closeModal} />
-                )}
-            </div>
+                    </div>
+                    <div className={'flex flex-col gap-y-8'}>
+                        <h1 className={'mx-2 text-lg font-extrabold'}>현직자 인증</h1>
+                        <SetCorpInfoSection
+                            selectedCorpInfo={selectedCorpInfo}
+                            updateSelectedCorpInfo={updateSelectedCorpInfo}
+                        />
+                        <div className={'flex justify-end'}>
+                            <p className={'text-[0.75rem] text-slate-500'}>
+                                찾으시는 도메인이 없으신가요?{' '}
+                                <button
+                                    className={
+                                        'font-bold text-plump-purple-600 hover:underline hover:underline-offset-4'
+                                    }
+                                    type={'button'}
+                                    onClick={() => {
+                                        setCurrentViewName('requestToAddDomainView');
+                                    }}
+                                >
+                                    도메인 추가 요청
+                                </button>
+                            </p>
+                        </div>
+                        {isCorpInfoSelected && (
+                            <EmployeeVerificationForm selectedCorpInfo={selectedCorpInfo} closeModal={closeModal} />
+                        )}
+                    </div>
+                </>
+            ) : (
+                <RequestToAddDomainView
+                    backToEmployeeVerificationView={() => {
+                        setCurrentViewName('employeeVerificationView');
+                    }}
+                    closeModal={closeModal}
+                />
+            )}
         </Modal>
     );
 }
