10e25dee5c3c3921815422511bc95cd97bcbc6ea
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-12 15:59:09 +0900

    Feat: 관리자 페이지 네비게이션 바 컴포넌트 추가

---
+++ src/Pages/admin/Components/AdminPageNavbar.tsx
@@ -0,0 +1,48 @@
+import { Link, NavLink } from 'react-router-dom';
+
+export default function AdminPageNavbar() {
+    const { VITE_ADMIN_SECRET_KEY } = import.meta.env;
+
+    return (
+        <nav
+            className={
+                'sticky top-0 z-50 flex h-[4.5rem] w-full min-w-[1500px] items-center justify-center border-b border-slate-300 backdrop-blur-md'
+            }
+        >
+            <div className={'w-[80rem]'}>
+                <div className={'flex w-full items-center gap-x-10'}>
+                    <Link
+                        className={
+                            'logo inline-block bg-gradient-to-br from-plump-purple-500 to-plump-purple-700 bg-clip-text text-2xl font-bold text-transparent'
+                        }
+                        to={'/'}
+                    >
+                        KoffeeChat
+                    </Link>
+                    <ul className={'flex gap-x-4'}>
+                        <li>
+                            <NavLink
+                                to={`/admin-${VITE_ADMIN_SECRET_KEY}/manage/domain`}
+                                className={({ isActive }) => {
+                                    return `rounded-lg px-2 py-2.5 text-[0.93rem] font-bold transition-all ${isActive ? 'bg-plump-purple-50 text-plump-purple-600' : 'text-slate-800 hover:bg-slate-100'}  `;
+                                }}
+                            >
+                                도메인 관리
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to={`/admin-${VITE_ADMIN_SECRET_KEY}/manage/post`}
+                                className={({ isActive }) => {
+                                    return `rounded-lg px-2 py-2.5 text-[0.93rem] font-bold transition-all ${isActive ? 'bg-plump-purple-50 text-plump-purple-600' : 'text-slate-800 hover:bg-slate-100'}  `;
+                                }}
+                            >
+                                게시글 관리
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
