8a137da6c1a84eff5e5461cc6693e796e02d5404
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-18 23:39:22 +0900

    Design: 403 에러 페이지 디자인 변경

--- src/Pages/ErrorPages/ForbiddenErrorPage.tsx
+++ src/Pages/ErrorPages/ForbiddenErrorPage.tsx
@@ -1,25 +1,28 @@
 import { Link } from 'react-router-dom';
-import { RiUserForbidLine } from 'react-icons/ri';
 
 export default function ForbiddenErrorPage() {
     return (
-        <div className={'my-20 flex flex-col items-center gap-y-8'}>
-            <div className={'flex items-center justify-center rounded-full bg-red-50 p-1.5 shadow-xl shadow-red-200'}>
-                <RiUserForbidLine className={'size-10 text-red-500'} />
+        <div className={'my-24 flex flex-col items-center gap-y-6'}>
+            <div className={'rounded-3xl bg-plump-purple-50 px-4 py-1'}>
+                <span className={'text-[2.3rem] font-[900] text-plump-purple-600'}>403</span>
             </div>
-            <h1 className={'text-[1.35rem] font-extrabold text-red-500'}>403 Forbidden</h1>
-            <div className={'flex flex-col items-center gap-y-6'}>
-                <p className={'text-center text-[0.95rem] font-bold leading-7 text-slate-500'}>
-                    접근이 거부되었습니다. 해당 페이지를 보기 위한 권한이 없습니다.
-                </p>
-                <Link
-                    to={'/'}
-                    className={
-                        'rounded-xl bg-slate-100 px-4 py-2 text-[0.95rem] font-extrabold transition-all hover:bg-slate-200'
-                    }
-                >
-                    메인 페이지
-                </Link>
+            <div className={'flex flex-col items-center gap-y-10'}>
+                <div className={'flex flex-col items-center gap-y-3'}>
+                    <h1 className={'text-xl font-extrabold text-slate-800'}>🤐 접근이 거부되었습니다.</h1>
+                    <p className={'text-center text-[0.9rem] font-bold text-slate-500'}>
+                        요청하신 페이지 접근이 거부되었습니다. 해당 페이지를 보기 위한 권한이 없습니다.
+                    </p>
+                </div>
+                <div>
+                    <Link
+                        to={'/'}
+                        className={
+                            'rounded-xl bg-slate-100 px-4 py-3 text-[0.9rem] font-bold transition-all hover:bg-slate-200'
+                        }
+                    >
+                        메인 페이지
+                    </Link>
+                </div>
             </div>
         </div>
     );
