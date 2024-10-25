ba2844f3618693b0dc951b1f28718407786b53a4
Kim Kangmin <rlarkdals1202@gmail.com>
2024-10-25 18:45:45 +0900

    Feat: 승인, 거절 상태 변경 관련 커스텀 훅 추가

---
+++ src/Pages/admin/manage/domain/Hooks/useUpdateStatusMutation.tsx
@@ -0,0 +1,36 @@
+import axios, { AxiosError } from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import TOAST_OPTIONS from '@/Constants/toastOptions.ts';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function useUpdateStatusMutation() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({ id, statusToUpdate }: { id: number; statusToUpdate: 'APPROVED' | 'REJECTED' }) =>
+            axios.patch(`/api/corps/admin/${id}`, statusToUpdate),
+
+        onSuccess: (_, variables) => {
+            toast.success(
+                <p className={'text-[0.85rem]'}>
+                    {variables.statusToUpdate === 'APPROVED' ? '승인' : '거절'}되었습니다.
+                </p>,
+                TOAST_OPTIONS,
+            );
+            queryClient.invalidateQueries({
+                queryKey: ['list', 'corpInfo'],
+            });
+        },
+
+        onError: (error: AxiosError) => {
+            console.error(error);
+            toast.error(
+                <p className={'text-[0.85rem] leading-relaxed'}>
+                    현재 요청을 처리할 수 없습니다. <br /> 잠시 후 다시 시도해주세요.
+                </p>,
+                TOAST_OPTIONS,
+            );
+        },
+    });
+}
