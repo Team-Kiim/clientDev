ef371585ad93dd41bc37799c868a38e992f55f00
Kim Kangmin <rlarkdals1202@gmail.com>
2024-11-03 16:09:50 +0900

    Refactor: 새로운 라우터 객체 import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
-import router from '@/router.tsx';
+import router from '@/Routes/router.tsx';
 
 Modal.setAppElement('#root');
 
