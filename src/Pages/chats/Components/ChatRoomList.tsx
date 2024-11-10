27a8e4bae07fb46006435e02fe84ceff85782624
Kim Kangmin <rlarkdals1202@gmail.com>
2024-11-10 16:59:06 +0900

    Feat: 기술 채팅방 목록 조회 기능 추가

--- src/Pages/chats/Components/ChatRoomList.tsx
+++ src/Pages/chats/Components/ChatRoomList.tsx
@@ -1,4 +1,6 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
 import ChatRoomListItem from '@/Pages/chats/Components/ChatRoomListItem.tsx';
+import fetchSkillChatRoomList from '@/Pages/chats/Utils/fetchSkillChatRoomList.ts';
 import { CATEGORIES } from '@/Constants/categories.ts';
 
 interface Props {
@@ -6,16 +8,26 @@ interface Props {
 }
 
 export default function ChatRoomList({ currentParentSkillCategory }: Props) {
-    const childCategoryList = CATEGORIES.find(
-        category => category.value === currentParentSkillCategory,
-    ).childCategories;
-
-    console.log(childCategoryList);
+    const { data } = useSuspenseQuery({
+        queryKey: ['chatRoomList', 'skill', { category: currentParentSkillCategory }],
+        queryFn: fetchSkillChatRoomList,
+    });
 
     return (
-        <ul className={'flex flex-col'}>
-            {Array.from(Array(5).keys()).map(e => (
-                <ChatRoomListItem key={e} />
+        <ul className={'grid grid-cols-2 gap-x-3 gap-y-5'}>
+            {data.map(skillChatRoom => (
+                <ChatRoomListItem
+                    chatRoomId={skillChatRoom.chatRoomId}
+                    parentSkillCategory={
+                        CATEGORIES.find(category => category.value === skillChatRoom.parentSkillCategory).label
+                    }
+                    childSkillCategory={
+                        CATEGORIES.find(
+                            category => category.value === skillChatRoom.parentSkillCategory,
+                        ).childCategories.find(category => category.value === skillChatRoom.childSkillCategory).label
+                    }
+                    currentMemberSize={skillChatRoom.currentMemberSize}
+                />
             ))}
         </ul>
     );
--- src/Pages/chats/Components/ChatRoomListItem.tsx
+++ src/Pages/chats/Components/ChatRoomListItem.tsx
@@ -1,39 +1,40 @@
 import jsSvg from '@/assets/svgs/skillCategory/java_script.svg';
 import { Link } from 'react-router-dom';
+import { LuUsers2 } from 'react-icons/lu';
+import type SkillChatRoom from '@/Types/skillChatRoom.ts';
 
-export default function ChatRoomListItem() {
+type Props = SkillChatRoom;
+
+export default function ChatRoomListItem({
+    chatRoomId,
+    parentSkillCategory,
+    childSkillCategory,
+    currentMemberSize,
+}: Props) {
     return (
-        <li className={'border-b border-slate-300 last:border-none hover:bg-slate-50'}>
-            <Link to={'/chat/java'}>
-                <div className={'flex flex-col gap-y-4 p-4 transition-all'}>
+        <li className={'rounded-2xl border border-slate-200 p-4 transition-all hover:bg-slate-50'}>
+            <Link to={`/chat/${chatRoomId}`}>
+                <div className={'flex flex-col gap-y-4 transition-all'}>
                     <div className={'flex items-center gap-x-3'}>
-                        <img className={'size-11 rounded-full object-contain'} src={jsSvg} alt={'js'} />
+                        <img className={'size-10 object-contain'} src={jsSvg} alt={'js'} />
                         <div className={'flex flex-col'}>
-                            <h2 className={'text-lg font-extrabold text-slate-800'}>JavaScript</h2>
-                            <span className={'text-[0.73rem] text-slate-500'}>#프로그래밍 언어</span>
+                            <h2 className={'text-[1.1rem] font-extrabold text-slate-800'}>{childSkillCategory}</h2>
+                            <div className={'rounded-lg bg-plump-purple-50 px-1 py-0.5'}>
+                                <span className={'text-[0.7rem] font-bold text-plump-purple-600'}>
+                                    #{parentSkillCategory}
+                                </span>
+                            </div>
                         </div>
                     </div>
-                    <div className={'flex w-full flex-1 flex-col '}>
-                        <div className={'flex w-full gap-x-1 text-[0.8rem]'}>
-                            <span className={'font-bold text-slate-600'}>asdf12</span>
-                            <span className={'line-clamp-1 text-slate-800'}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,{' '}
-                            </span>
-                        </div>
-                        <div className={'flex w-full gap-x-1 text-[0.8rem]'}>
-                            <span className={'font-bold text-neutral-600'}>asdf12</span>
-                            <span className={'line-clamp-1 text-slate-800'}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,{' '}
-                            </span>
-                        </div>
-                    </div>
-                    <div className={'flex items-center justify-end gap-x-2 text-slate-500'}>
+                    <div
+                        className={
+                            'ml-auto flex w-fit items-center justify-end gap-x-2 rounded-lg bg-slate-100 px-1 py-0.5 text-slate-700'
+                        }
+                    >
+                        <LuUsers2 className={'size-5'} />
                         <span className={'text-[0.7rem]'}>
-                            <span className={'font-bold'}>500명</span> 참여 중
+                            <span className={'font-bold'}>{currentMemberSize}명</span> 참여 중
                         </span>
-                        •<span className={'text-[0.7rem]'}>방금 전</span>
                     </div>
                 </div>
             </Link>
