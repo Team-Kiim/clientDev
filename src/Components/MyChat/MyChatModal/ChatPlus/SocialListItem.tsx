972836dc88fa79aedf80e33bc21c32bfc691b9b0
Kim Kangmin <rlarkdals1202@gmail.com>
2024-11-10 17:28:42 +0900

    Feat: 1대1 채팅 추가 기능 구현 완료

--- src/Components/MyChat/MyChatModal/ChatPlus/SocialListItem.tsx
+++ src/Components/MyChat/MyChatModal/ChatPlus/SocialListItem.tsx
@@ -1,25 +1,37 @@
-import { faker } from '@faker-js/faker';
-
-interface Props {
-    onChatButtonClick(): void;
+interface SocialInfo {
+    memberId: string;
+    email: string;
+    nickname: string;
+    profileImageUrl: string;
+    loginMember: boolean;
+    followedByLoginMember: boolean;
 }
 
-export default function SocialListItem({ onChatButtonClick }: Props) {
+interface Props {
+    socialInfo: SocialInfo;
+    onChatButtonClick(memberId: string, otherUserNickname: string): void;
+}
+
+export default function SocialListItem({ socialInfo, onChatButtonClick }: Props) {
+    const { email, profileImageUrl, memberId, nickname } = socialInfo;
+
     return (
         <li className={'flex gap-x-4 border-b border-slate-200 px-4 py-3'}>
             <div className={'avatar size-10 rounded-full'}>
-                <img className={'size-10 rounded-full'} src={faker.image.avatarGitHub()} alt={'profileImg'} />
+                <img className={'size-10 rounded-full'} src={profileImageUrl} alt={'profileImg'} />
             </div>
             <div className={'flex flex-1 flex-col'}>
-                <span className={'line-clamp-1 text-[0.9rem] font-extrabold'}>asdf1202</span>
-                <span className={'text-[0.75rem] text-slate-500'}>kkangasdf12@naver.com</span>
+                <span className={'line-clamp-1 text-[0.9rem] font-extrabold'}>{nickname}</span>
+                <span className={'text-[0.75rem] text-slate-500'}>{email}</span>
             </div>
             <div className={'flex flex-col justify-center'}>
                 <button
                     className={
                         'rounded-3xl border-2 border-neutral-800 px-2 py-1 text-[0.7rem] font-extrabold transition-all hover:bg-slate-100'
                     }
-                    onClick={onChatButtonClick}
+                    onClick={() => {
+                        onChatButtonClick(memberId, nickname);
+                    }}
                     type={'button'}
                 >
                     채팅
--- src/Components/MyChat/MyChatModal/ChatPlus/SocialListWithFilter.tsx
+++ src/Components/MyChat/MyChatModal/ChatPlus/SocialListWithFilter.tsx
@@ -1,15 +1,38 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import { useState } from 'react';
 import NickNameSearchInput from '@/Components/MyChat/MyChatModal/ChatPlus/NickNameSearchInput.tsx';
 import SocialFilter from '@/Components/MyChat/MyChatModal/ChatPlus/SocialFilter.tsx';
 import SocialListItem from '@/Components/MyChat/MyChatModal/ChatPlus/SocialListItem.tsx';
+import fetchSocialMediaUserList from '@/Pages/user/social/Utils/fetchSocialMediaUserList.ts';
+import { useQueryClient, useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 interface Props {
     updateCurrentViewName(viewName: string): void;
 }
 
 export default function SocialListWithFilter({ updateCurrentViewName }: Props) {
+    const queryClient = useQueryClient();
+
     const [currentFilter, setCurrentFilter] = useState('following');
     const [nickNameToSearch, setNickNameToSearch] = useState('');
+
+    const { data } = useSuspenseInfiniteQuery({
+        queryKey: [
+            'user',
+            'social',
+            {
+                relationshipType: currentFilter === 'following' ? 'followings' : 'followers',
+                keyword: nickNameToSearch,
+                memberId: null,
+            },
+        ],
+        queryFn: fetchSocialMediaUserList,
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, _, lastPageParam) => (lastPage.length < 16 ? undefined : lastPageParam + 1),
+    });
+
+    const socialList = data.pages.flat();
 
     const updateNickNameToSearch = (nickName: string) => {
         setNickNameToSearch(nickName);
@@ -19,9 +42,33 @@ export default function SocialListWithFilter({ updateCurrentViewName }: Props) {
         setCurrentFilter(filter);
     };
 
-    const handleChatButtonClick = () => {
-        // 채팅방 만듬 -> 응답받은 아이디 가지고 채팅방 들어감
-        updateCurrentViewName('chatRoom20');
+    const handleChatButtonClick = async (memberId: string, otherUserNickname: string) => {
+        try {
+            const { chatRoomId } = await axios
+                .post('/api/direct-chat-room/', {
+                    memberId,
+                })
+                .then(response => response.data);
+            queryClient.invalidateQueries({
+                queryKey: ['user', 'chatRoomList'],
+                refetchType: 'all',
+            });
+            updateCurrentViewName(
+                `chatRoom:${chatRoomId} oppositeMemberId:${memberId} otherUserName:${otherUserNickname}`,
+            );
+        } catch (error) {
+            Swal.fire({
+                target: '.myChatModal',
+                html: '<p class="leading-4 text-slate-500 text-[0.75rem]">현재 채팅을 할 수 없습니다.<br/> 잠시 후 다시 시도해주세요.</p>',
+                showCancelButton: false,
+                confirmButtonText: '확인',
+                customClass: {
+                    popup: 'w-[240px] text-[0.75rem] py-3',
+                    container: 'rounded-3xl absolute',
+                    confirmButton: 'text-[0.8rem] bg-plump-purple-600 rounded-xl',
+                },
+            });
+        }
     };
 
     return (
@@ -30,11 +77,27 @@ export default function SocialListWithFilter({ updateCurrentViewName }: Props) {
                 <NickNameSearchInput updateNickNameToSearch={updateNickNameToSearch} />
                 <SocialFilter currentFilter={currentFilter} updateSocialFilter={updateSocialFilter} />
             </div>
-            <div className={'flex flex-1 shrink-0 flex-grow flex-col overflow-y-auto overscroll-y-contain'}>
-                {Array.from(Array(10).keys()).map(element => {
-                    return <SocialListItem key={element} onChatButtonClick={handleChatButtonClick} />;
-                })}
-            </div>
+            {socialList.length !== 0 ? (
+                <ul className={'flex flex-1 shrink-0 flex-grow flex-col overflow-y-auto overscroll-y-contain'}>
+                    {socialList.map(socialListItem => {
+                        return (
+                            <SocialListItem
+                                key={socialListItem.memberId}
+                                socialInfo={socialListItem}
+                                onChatButtonClick={handleChatButtonClick}
+                            />
+                        );
+                    })}
+                </ul>
+            ) : (
+                <div className={'flex min-h-0 flex-1 items-center justify-center'}>
+                    <p className={'my-10 text-center text-[0.8rem] text-slate-400'}>
+                        {currentFilter === 'following'
+                            ? '아직 팔로우한 사용자가 없어요'
+                            : '아직 팔로우 하는 사용자가 없어요.'}
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
