d94de3524c1b31744fe0312b6cca437a532e450f
Kim Kangmin <rlarkdals1202@gmail.com>
2024-11-11 22:43:18 +0900

    Feat: QnA 게시글 페이지로 이동 후 맨 위로 스크롤 하도록 변경

--- src/Pages/qnas/[boardId]/page.tsx
+++ src/Pages/qnas/[boardId]/page.tsx
@@ -1,7 +1,10 @@
 import { Suspense } from 'react';
 import PostView from '@/Pages/qnas/[boardId]/Components/PostView.tsx';
+import useScrollToTop from '@/Hooks/useScrollToTop.ts';
 
 export default function Page() {
+    useScrollToTop();
+
     return (
         <div className={'mt-8 flex w-full min-w-[1500px] justify-center'}>
             <div className={'w-[45rem]'}>
