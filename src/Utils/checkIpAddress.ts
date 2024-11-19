88424322b7cffe4819b705dae6b293139fd81b80
Kim Kangmin <rlarkdals1202@gmail.com>
2024-11-19 17:10:25 +0900

    Remove: 사용되지 않는 파일 삭제

--- src/Utils/checkIpAddress.ts
+++
@@ -1,8 +0,0 @@
-const checkIpAddress = (domain: string): boolean => {
-    const regexForTest =
-        /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-
-    return regexForTest.test(domain);
-};
-
-export default checkIpAddress;
